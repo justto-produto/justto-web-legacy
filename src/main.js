9d2f9bfa698611f55cae5ce0ca5b99df3100130d
Henrique Liberato <henriquebmx@gmail.com>
2018-08-16 20:32:40 -0300

    add new pckages

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import './registerServiceWorker'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
