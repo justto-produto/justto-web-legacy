2b43f2cf3ca66ff25f41244c5198bf341a9656c2
Henrique Liberato <henriquebmx@gmail.com>
2018-09-12 18:54:46 -0300

    Import page

--- src/views/content/Import.vue
+++ src/views/content/Import.vue
@@ -34,15 +34,7 @@
         Melhores práticas para importar casos
       </h3>
       <br>
-      <p>
-        Ao escolher uma opção para importação, certifique-se de que os dados estão inseridos corretamente. Dessa forma,
-         o sistema irá reconhecer mais rapidamente os dados, e além disso, enriquecer com informações complementares
-         colhidas diretamente do TJ.<br><br>
-        Não se esqueça de preencher os dados mínimos da planilha: <br><br>
-        • Número do processo<br>
-        • Empresa<br>
-        • Alçadas da negociação <br>
-        • Nome e CPF das partes
+      <p>Ao escolher uma opção para importação, certifique-se de que os dados estão inseridos corretamente. Dessa forma, o sistema irá reconhecer mais rapidamente os dados, e além disso, enriquecer com informações complementares colhidas diretamente do TJ.<br><br> Não se esqueça de preencher os dados mínimos da planilha <br><br> • Número do processo<br> • Empresa<br> • Alçadas da negociação <br> • Nome e CPF das partes
       </p>
     </template>
   </content-view>
