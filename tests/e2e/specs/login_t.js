1a086c1f32d5b2767c69993927ab2cf5588800ac
jus-grazi <graziele@justto.com.br>
2018-11-22 15:55:10 -0200

    Adicionando testes do Cypress

---
+++ tests/e2e/specs/login_t.js
@@ -0,0 +1,38 @@
+/// <reference types="Cypress" />
+
+context('Actions', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8081/login')
+  })
+
+  // Acessando página de Login do justto-web
+
+  it('Acessando Página de Login', () => {
+    // https://on.cypress.io/type
+    cy.get('[class=el-input__inner]').then($els => $els.get(0))
+      .type('fake@email.com')
+
+    cy.get('[class=el-input__inner]').then($els => $els.get(1))
+      .type('password')
+    // Press 'Enter' buttom
+    //cy.contains('Entrar').click()
+  })
+
+  it('Cadastrar usuário', () => {
+  // https://on.cypress.io/type
+    cy.contains('Cadastre').click()
+
+    // Fills 'Nome' field
+      cy.get('[class=el-input__inner]').then($els => $els.get(2))
+        .type('nome')
+    // Fills 'Email' field
+      cy.get('[class=el-input__inner]').then($els => $els.get(1))
+        .type('e@mail.com')
+    // Fills 'Senha' field
+      cy.get('[class=el-input__inner]').then($els => $els.get(2))
+        .type('password')
+
+  cy.contains('Cadastrar').click()
+    
+  })
+})
