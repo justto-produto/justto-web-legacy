087abd5bcb30c6dc1207032cf03074dea0c1372a
Henrique Liberato <henriquebmx@gmail.com>
2019-02-15 17:07:01 -0200

    Ativa plugin de socket

--- src/plugins/socket.js
+++ src/plugins/socket.js
@@ -1,30 +1,30 @@
-// import Vue from 'vue'
-// import { Client } from '@stomp/stompjs' // Message
-// import store from '../store'
-//
-// const VueStompJs = {
-//   install (Vue, options) {
-//     const client = new Client()
-//     client.brokerURL = options.brokerURL
-//     // client.onConnect = function (frame) {
-//     //   console.info('Connected at ' + client.brokerURL)
-//     // }
-//     client.onStompError = function (frame) {
-//       console.error('Broker reported error: ' + frame.headers['message'])
-//       console.error('Additional details: ' + frame.body)
-//     }
-//     client.activate()
-//     Vue.prototype.$stomp = {
-//       subscribe (workspace) {
-//         client.subscribe('/whatsapp/refresh/' + workspace, function (message) {
-//           store.commit('setMessage', JSON.parse(message.body))
-//         })
-//       },
-//       unsubscribe () {
-//         client.unsubscribe()
-//       }
-//     }
-//   }
-// }
-//
-// Vue.use(VueStompJs, { brokerURL: 'ws://homol.justto.com.br/workspace/websocket' })
+import Vue from 'vue'
+import { Client } from '@stomp/stompjs' // Message
+import store from '../store'
+
+const VueStompJs = {
+  install (Vue, options) {
+    const client = new Client()
+    client.brokerURL = options.brokerURL
+    // client.onConnect = function (frame) {
+    //   console.info('Connected at ' + client.brokerURL)
+    // }
+    client.onStompError = function (frame) {
+      console.error('Broker reported error: ' + frame.headers['message'])
+      console.error('Additional details: ' + frame.body)
+    }
+    client.activate()
+    Vue.prototype.$stomp = {
+      subscribe (workspace) {
+        client.subscribe('/whatsapp/refresh/' + workspace, function (message) {
+          store.commit('setMessage', JSON.parse(message.body))
+        })
+      },
+      unsubscribe () {
+        client.unsubscribe()
+      }
+    }
+  }
+}
+
+Vue.use(VueStompJs, { brokerURL: 'ws://homol.justto.com.br/workspace/websocket' })
