09879c4d03bd6324c74ddca337a33639c0c3ba66
Bruno Moreno <bruno@justto.com.br>
2019-03-18 08:09:03 -0300

    MIS-187 Retorna apontamento para Homolog.

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -8,8 +8,8 @@ if (AUTH_TOKEN) {
 }
 let config = {
   // baseURL: process.env.baseURL || process.env.apiUrl || '/',
-  // baseURL: process.env.baseURL || process.env.apiUrl || 'http://homol.justto.com.br/',
-  baseURL: process.env.baseURL || process.env.apiUrl || 'https://justto.app/',
+  baseURL: process.env.baseURL || process.env.apiUrl || 'http://homol.justto.com.br/',
+  // baseURL: process.env.baseURL || process.env.apiUrl || 'https://justto.app/',
   timeout: 60 * 100000,
   headers: {
   }
