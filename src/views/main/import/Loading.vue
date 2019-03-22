f1087be6d414d300d4bc7726b4e469aa691bc060
Henrique Liberato <henriquebmx@gmail.com>
2019-03-21 22:56:12 -0300

    MIS-254 Adiciona timeout às animações

--- src/views/main/import/Loading.vue
+++ src/views/main/import/Loading.vue
@@ -2,28 +2,38 @@
   <div class="import-loading-view">
     <h1>Saiba os próximos passos</h1>
     <div class="import-loading-view__gifs">
-      <div class="import-loading-view__gif">
-        <img src="@/assets/gifs/strategy.gif">
-        <h3>Campanha<br>configurada</h3>
-      </div>
-      <div class="import-loading-view__gif">
-        <img src="@/assets/gifs/search.gif">
-        <h3>Enriquecimento<br>de dados</h3>
-      </div>
-      <div class="import-loading-view__gif">
-        <img src="@/assets/gifs/send.gif">
-        <h3>Envio de<br>mensagens</h3>
-      </div>
-      <div class="import-loading-view__gif">
-        <img src="@/assets/gifs/message.gif">
-        <h3>Comece a<br>negociar</h3>
-      </div>
-      <div class="import-loading-view__gif">
-        <img src="@/assets/gifs/deal.gif">
-        <h3>Feche<br>acordos</h3>
-      </div>
+      <transition name="el-zoom-in-center">
+        <div v-show="showGif > 0" class="import-loading-view__gif">
+          <img src="@/assets/gifs/strategy.gif">
+          <h3>Campanha<br>configurada</h3>
+        </div>
+      </transition>
+      <transition name="el-zoom-in-center">
+        <div v-show="showGif > 1" class="import-loading-view__gif">
+          <img src="@/assets/gifs/search.gif">
+          <h3>Enriquecimento<br>de dados</h3>
+        </div>
+      </transition>
+      <transition name="el-zoom-in-center">
+        <div v-show="showGif > 2" class="import-loading-view__gif">
+          <img src="@/assets/gifs/send.gif">
+          <h3>Envio de<br>mensagens</h3>
+        </div>
+      </transition>
+      <transition name="el-zoom-in-center">
+        <div v-show="showGif > 3" class="import-loading-view__gif">
+          <img src="@/assets/gifs/message.gif">
+          <h3>Comece a<br>negociar</h3>
+        </div>
+      </transition>
+      <transition name="el-zoom-in-center">
+        <div v-show="showGif > 4" class="import-loading-view__gif">
+          <img src="@/assets/gifs/deal.gif">
+          <h3>Feche<br>acordos</h3>
+        </div>
+      </transition>
     </div>
-    <el-button type="primary" :plain="loading" :loading="loading" @click="$router.push('/management')">
+    <el-button type="primary" :plain="showGif < 5" :loading="showGif < 5" @click="$router.push('/management')">
       Continuar
     </el-button>
   </div>
@@ -34,14 +44,31 @@ export default {
   name: 'ImportLoading',
   data () {
     return {
-      loading: true
+      showGif: 0
     }
   },
-  beforeMount() {
-    let self = this
-    setTimeout(function () {
-      self.loading = false
-    }, 5000)
+  mounted () {
+    this.increaseShow()
+  },
+  methods: {
+    increaseShow () {
+      let self = this
+      if (self.showGif === 0) {
+        setTimeout(function () {
+          self.showGif++
+          if (self.showGif < 5) {
+            self.increaseShow()
+          }
+        }, 1000)
+      } else {
+        setTimeout(function () {
+          self.showGif++
+          if (self.showGif < 5) {
+            self.increaseShow()
+          }
+        }, 2000)
+      }
+    }
   }
 }
 </script>
