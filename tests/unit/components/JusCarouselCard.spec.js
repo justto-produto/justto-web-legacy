1beaed1fc4fdf29392a3a0994bbdb9fc88dd2ea5
Henrique Liberato <henriquebmx@gmail.com>
2019-06-19 16:39:36 -0300

    SAAS-449 Correge testes de components

--- tests/unit/components/JusCarouselCard.spec.js
+++ tests/unit/components/JusCarouselCard.spec.js
@@ -2,19 +2,20 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import JusCarouselCard from '@/components/layouts/JusCarouselCard.vue'
 import VueRouter from 'vue-router'
 import Element from 'element-ui'
+import '@/filters/capitalize'
 
 const localVue = createLocalVue()
 localVue.use(VueRouter)
 localVue.use(Element)
 
+let router = new VueRouter()
+const wrapper = shallowMount(JusCarouselCard, { localVue, router })
+
 describe('JusCarouselCard.vue', () => {
-  let router = new VueRouter()
   it('É um objeto Vue.', () => {
-    const wrapper = shallowMount(JusCarouselCard, { localVue, router })
     expect(wrapper.isVueInstance()).toBe(true)
   })
   it('Inicia vazio', () => {
-    const wrapper = shallowMount(JusCarouselCard, { localVue, router })
-    expect(wrapper.props().slide).toBe({})
+    expect(!!Object.keys(wrapper.props().slide)).toBe(true)
   })
 })
