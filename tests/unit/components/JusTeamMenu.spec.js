bdee27492a9cc586cbfc5df0c3f99f546f92ae8b
Elize <elizete_srm@outlook.com>
2019-06-20 15:22:11 -0300

    SAAS-456 Cria teste para componente JusViewMain.vue

---
+++ tests/unit/components/JusTeamMenu.spec.js
@@ -0,0 +1,19 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import JusTeamMenu from '@/components/layouts/JusTeamMenu.vue'
+import Element from 'element-ui'
+import '@/filters/capitalize'
+import Vuex from 'vuex'
+import JusAvatarUser from '@/components/images/JusAvatarUser.vue'
+
+const localVue = createLocalVue()
+localVue.use(Element)
+localVue.component('JusAvatarUser', JusAvatarUser)
+localVue.use(Vuex)
+
+describe('JusTeamMenu.vue', () => {
+  let store
+  it('É um objeto Vue.', () => {
+    const wrapper = shallowMount(JusTeamMenu, { localVue, store })
+    expect(wrapper.isVueInstance()).toBe(true)
+  })
+})
