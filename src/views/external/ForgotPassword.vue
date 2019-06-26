2cabfd048b7a96908c849eb81281af9b951f1149
Guilherme Rios <guilherme.rc.98@gmail.com>
2019-06-26 16:53:07 -0300

    SAAS-466 Cria testes para 'Esqueci minha senha'

--- src/views/external/ForgotPassword.vue
+++ src/views/external/ForgotPassword.vue
@@ -19,15 +19,10 @@
             v-show="showSuccess"
             title="Requisição de recuperação enviada com sucesso! Acesse seu email para prosseguir."
             type="success"
-<<<<<<< HEAD
-            @close="showSuccess = false"
-            data-testid="showSucess"/>
-=======
-            data-testid="showSucess"
-            @close="showSuccess = false" />
->>>>>>> d203b33799ea528e2635b4247337ab222a97b958
+            data-testid="forgot-password-success"
+            @close="showSuccess = false"/>
           <el-form-item label="Email" prop="email">
-            <el-input v-model="forgotForm.email" data-testid="forgot-email"/>
+            <el-input v-model="forgotForm.email" data-testid="forgot-password-email"/>
           </el-form-item>
           <el-button
             native-type="submit"
