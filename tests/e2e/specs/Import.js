68a5632cfefa5d248354a1508e3d7c2a58b2de17
jus-grazi <graziele@justto.com.br>
2019-07-02 18:24:59 -0300

    Adiciona teste de gerenciamento/engajamento

--- tests/e2e/specs/Import.js
+++ tests/e2e/specs/Import.js
@@ -1,5 +1,4 @@
 describe('Justto.App - Planilha Modelo', function() {
-
     it('Login: Sucesso', function() {
         // Acessa a página inicial do Justto.App
         // cy.visit('http://homol.justto.com.br')
@@ -23,31 +22,6 @@ describe('Justto.App - Planilha Modelo', function() {
 
         // Valida se acesso foi feito
         cy.url().should('include', '/#/management')
-    })
-
-    it('Historico de Importações: Vazio', function() {
-        // Acessa a tela de gerenciamento
-        cy.get('[data-testid=menu-import]').click()
-
-        // Sistema deve redirecionar para a página de Importação
-        cy.url().should('include', '/#/import')
-
-        // Verifica se Histórico de Importações esta vazio
-        cy.get('[data-testid=empty-history]')
-            .contains('Aqui você encontra o registro de importações no sistema. Por enquanto, você não possui importações.')
-            .should('be.visible')
-    })
-
-    it('Baixa Planilha Modelo: Sucesso', function() {
-        // verifica se o botão está visível
-        cy.get('[data-testid=download-model]')
-            .should('be.visible')
-            .click()
-    })
-
-    it('Planilha modelo Disponível: Sucesso', function() {
-        // Verifica se link está disponível
-        cy.request('localhost:8080/Planilha-Modelo-Justto.xlsx')
     })
 
     // IMPORTA PLANILHA ===========================================
