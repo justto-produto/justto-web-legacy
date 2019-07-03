70bfabc8b3e8c38c1daf89a6cd084fac595a6218
jus-grazi <graziele@justto.com.br>
2019-07-03 16:07:06 -0300

    SAAS-470 Testes de visualização em Gerenciamento

--- tests/e2e/specs/management/AllVisualization.js
+++ tests/e2e/specs/management/AllVisualization.js
@@ -1,5 +1,5 @@
-describe('Justto.App - Gerenciamento: Visualização Engajamento', function () {
-  it('Gerenciamento: Engajamento - Exibição dos Casos - Vazio', function () {
+describe('Justto.App - Gerenciamento: Visualização Todos', function () {
+  it('Gerenciamento: Todos - Exibição dos Casos - Vazio', function () {
     // Acessa a página inicial do Justto.App
     // cy.visit('http://homol.justto.com.br')
     cy.visit('localhost:8080')
@@ -12,27 +12,27 @@ describe('Justto.App - Gerenciamento: Visualização Engajamento', function () {
       .type('dubu@voemail.com')
       .should('have.value', 'dubu@voemail.com')
 
-    // Preenche o campo 'Senha'
+      // Preenche o campo 'Senha'
     cy.get('[data-testid=login-password]')
       .type('password')
       .should('have.value', 'password')
 
-    // Clica no botão "Entrar"
+      // Clica no botão "Entrar"
     cy.get('[data-testid=submit]')
       .click()
 
-    // Verifica se tela acessada é a de "Gerenciamento"
+      // Verifica se tela acessada é a de "Gerenciamento"
     cy.url().should('include', '/#/management')
 
-    // Seleciona a aba "Engajamento"
-    cy.contains('Engajamento').should('be.visible').click()
+    // Seleciona a aba "Todos"
+    cy.contains('Todos').should('be.visible').click()
 
     // Verifica se mensagem "Não foram encontradas disputas" é exibida
     cy.contains('Não foram encontradas disputas')
       .should('be.visible')
   })
 
-  it('Gerenciamento: Engajamento - Exibição de Casos - Com casos', function () {
+  it('Gerenciamento: Todos - Exibição de Casos - Com casos', function () {
     // Acessa a página inicial do Justto.App
     // cy.visit('http://homol.justto.com.br')
     cy.visit('localhost:8080')
@@ -45,23 +45,28 @@ describe('Justto.App - Gerenciamento: Visualização Engajamento', function () {
       .type('zozuyakip@royalhost.info')
       .should('have.value', 'zozuyakip@royalhost.info')
 
-    // Preenche o campo 'Senha'
+      // Preenche o campo 'Senha'
     cy.get('[data-testid=login-password]')
       .type('password')
       .should('have.value', 'password')
 
-    // Clica no botão "Entrar"
+      // Clica no botão "Entrar"
     cy.get('[data-testid=submit]')
       .click()
 
-    // Verifica se tela acessada é a de "Gerenciamento"
+      // Verifica se tela acessada é a de "Gerenciamento"
     cy.url().should('include', '/#/management')
 
-    // Seleciona a aba "Engajamento"
-    cy.contains('Engajamento').should('be.visible')
+    // Seleciona a aba "Todos"
+    cy.contains('Todos').should('be.visible')
       .click()
 
-    // Verifica se existem casos exibidos
+      // Verifica se existem casos exibidos
     cy.get('[class=el-table__body]').should('be.visible')
+
+    cy.wait(5000)
   })
 })
+
+
+
