d8966c61574128c7140be71a01c91ed4d2322ba3
Guilherme Rios <guilherme.rc.98@gmail.com>
2019-07-04 17:28:15 -0300

    SAAS-497 Adequa teste de login e esqueci minha senha para Toke MASTER

--- tests/e2e/specs/register/Register.js
+++ tests/e2e/specs/register/Register.js
@@ -1,86 +1,121 @@
 describe('Justto.App - Register', function () {
-  it('Registro: Sucesso', function () {
-    // Acessa a página inicial do Justto.App
-    // cy.visit('http://homol.justto.com.br')
-    cy.visit('localhost:8080')
+    it('Registro: Sucesso', function () {
+        // Acessa a página inicial do Justto.App
+        // cy.visit('http://homol.justto.com.br')
+        cy.visit('localhost:8080')
 
-    // Acessa a página de registro
-    cy.get('[data-testid=register]').click()
+        // Acessa a página de registro
+        cy.get('[data-testid=register]').click()
 
-    // Sistema deve redirecionar para a página de Registro
-    cy.url().should('include', '/#/register')
+        // Sistema deve redirecionar para a página de Registro
+        cy.url().should('include', '/#/register')
 
-    // Preenche o campo 'Nome'
-    cy.get('[data-testid=register-name]')
-      .type('Um Nome')
-      .should('have.value', 'Um Nome')
+        // Preenche o campo 'Nome'
+        cy.get('[data-testid=register-name]')
+            .type('Conta Teste')
+            .should('have.value', 'Conta Teste')
 
-    function randomEmail (range) {
-      var allLetters = '0123456789abcdefghiklmnopqrstuvwxyz'
-      var randomName = ''
-      for (var i = 0; i < range; i++) {
-        var randomNumber = Math.floor(Math.random() * allLetters.length)
-        randomName += allLetters.substring(randomNumber, randomNumber + 1)
-      }
-      var randomEmail = randomName + '@teste2e.com'
-      return randomEmail
-    }
+        function randomEmail () {
+            var randomEmail = new Date().getTime() + '@teste2e.com'
+            return randomEmail
+        }
 
-    // Preenche o campo 'Email'
-    cy.get('[data-testid=register-email]')
-      .type(randomEmail(20))
-    // .should('have.value', 'this.randomEmail')
+        // Preenche o campo 'Email'
+        cy.get('[data-testid=register-email]')
+            .type(randomEmail())
+            // .type('teste.onboarding@justto.com.br')
+            // .should('have.value', 'teste.onboarding@justto.com.br')
 
-    // Preenche o campo 'Senha'
-    cy.get('[data-testid=register-password]')
-      .type('password')
-      .should('have.value', 'password')
+        // Preenche o campo 'Senha'
+        cy.get('[data-testid=register-password]')
+            .type('password')
+            .should('have.value', 'password')
 
-    // Clica no botão "Entrar"
-    cy.get('[data-testid=submit]')
-      .click()
+        // Clica no botão "Entrar"
+        cy.get('[data-testid=submit]')
+            .click()
 
-    // Valida se registro funcionou
-    cy.get('[data-testid=register-success]')
-      .should('be.visible')
-    cy.get('[data-testid=register-success]')
-      .contains('Cadastro realizado com sucesso! Acesse seu email para prosseguir.')
-      .should('be.visible')
-  })
+        // Valida se registro funcionou
+        cy.get('[data-testid=register-success]')
+            .should('be.visible')
+        cy.get('[data-testid=register-success]')
+            .contains('Cadastro realizado com sucesso! Acesse seu email para prosseguir.')
+            .should('be.visible')
+    })
 
-  it('Registro: Email já Existente', function () {
-    // Acessa a página de login
-    cy.visit('localhost:8080')
+    it('Registro: Sucesso', function () {
+        // Acessa a página inicial do Justto.App
+        // cy.visit('http://homol.justto.com.br')
+        cy.visit('localhost:8080')
 
-    // Acessa a página de registro
-    cy.get('[data-testid=register]').click()
+        // Acessa a página de registro
+        cy.get('[data-testid=register]').click()
 
-    // Sistema deve redirecionar para a página de Registro
-    cy.url().should('include', '/#/register')
+        // Sistema deve redirecionar para a página de Registro
+        cy.url().should('include', '/#/register')
 
-    // Preenche o campo 'Nome'
-    cy.get('[data-testid=register-name]')
-      .type('Um Nome')
-      .should('have.value', 'Um Nome')
+        // Preenche o campo 'Nome'
+        cy.get('[data-testid=register-name]')
+            .type('Conta Teste')
+            .should('have.value', 'Conta Teste')
 
-    // Preenche   campo 'Email' com email não existente
-    cy.get('[data-testid=register-email]')
-      .type('gugo@eyeemail.com')
-      .should('have.value', 'gugo@eyeemail.com')
+        function randomEmail () {
+            var randomEmail = new Date().getTime() + '@teste2e.com'
+            return randomEmail
+        }
 
-    // Preenche o campo 'Senha'
-    cy.get('[data-testid=register-password')
-      .type('password')
-      .should('have.value', 'password')
+        // Preenche o campo 'Email'
+        cy.get('[data-testid=register-email]')
+            .type('email invalido')
+            .should('have.value', 'email invalido')
 
-    // Clica no botão "Entrar"
-    cy.get('[data-testid=submit]').click()
+        // Preenche o campo 'Senha'
+        cy.get('[data-testid=register-password]')
+            .type('password')
+            .should('have.value', 'password')
 
-    // Valida se login falhou
-    cy.url().should('include', '/#/register')
+        // Clica no botão "Entrar"
+        cy.get('[data-testid=submit]')
+            .click()
 
-    // Verifica de menssagem de erro foi exibida
-    cy.get('[data-testid=register-failure]')
-      .should('be.visible')
-  })
+        // Valida se registro funcionou
+        cy.contains('Insira um e-mail válido')
+            .should('be.visible')
+    })
+
+    it('Registro: Email já Existente', function () {
+        // Acessa a página de login
+        cy.visit('localhost:8080')
+
+        // Acessa a página de registro
+        cy.get('[data-testid=register]').click()
+
+        // Sistema deve redirecionar para a página de Registro
+        cy.url().should('include', '/#/register')
+
+        // Preenche o campo 'Nome'
+        cy.get('[data-testid=register-name]')
+          .type('Bruno Gatão')
+          .should('have.value', 'Bruno Gatão')
+
+        // Preenche   campo 'Email' com email não existente
+        cy.get('[data-testid=register-email]')
+          .type('bruno@justto.com.br.wks')
+          .should('have.value', 'bruno@justto.com.br.wks')
+
+        // Preenche o campo 'Senha'
+        cy.get('[data-testid=register-password')
+          .type('password')
+          .should('have.value', 'password')
+
+        // Clica no botão "Entrar"
+        cy.get('[data-testid=submit]').click()
+
+        // Valida se login falhou
+        cy.url().should('include', '/#/register')
+
+        // Verifica de menssagem de erro foi exibida
+        cy.get('[data-testid=register-failure]')
+          .should('be.visible')
+    })
 })
