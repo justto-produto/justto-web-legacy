7e5443ad867546dbd8c028ff78987c1cd550f5f0
Guilherme Rios <guilherme.rc.98@gmail.com>
2019-07-04 17:34:51 -0300

    SAAS-497 Valida email com token Master

--- tests/e2e/specs/register/Register.js
+++ tests/e2e/specs/register/Register.js
@@ -118,4 +118,13 @@ describe('Justto.App - Register', function () {
         cy.get('[data-testid=register-failure]')
           .should('be.visible')
     })
+
+    it('Ativação de email: Sucesso', function () {
+        // Acessa link de validação (Link do email)
+        cy.visit('localhost:8080/#/login?token=TSTAUTO')
+
+        // Sistema deve direcionar para página de login com token
+        cy.url('include', '/#/login?token=TSTAUTO')
+
+    })
 })
