b1586eee61001d4a5b2b15747aadb1276070ac63
jus-grazi <graziele@justto.com.br>
2019-07-11 16:35:15 -0300

    Corrige Testes de Notas da Disputa

--- tests/e2e/specs/management/BatchActions.js
+++ tests/e2e/specs/management/BatchActions.js
@@ -4,7 +4,7 @@ describe('Justto.App - Gerenciamento: Ação em Lote', function () {
     // cy.visit('http://homol.justto.com.br')
     cy.visit('localhost:8080')
 
-    // Sistema deve redirecionar para a página de Login
+    // Valida se o endereço redirecionado é o 'Login'
     cy.url().should('include', '/#/login')
 
     // Preenche o campo 'Email'
@@ -26,6 +26,21 @@ describe('Justto.App - Gerenciamento: Ação em Lote', function () {
   })
 
   it('Ação em Lote: Ganhar', function () {
+    // Seleciona a aba "Todos"
+    cy.contains('Todos').should('be.visible').click()
+
+    cy.get('.is-leaf > .cell > .el-checkbox > .el-checkbox__input > .el-checkbox__inner')
+      .click()
+
+    cy.get('[data-testid=batch-settled]').click()
+
+    cy.contains('Tem certeza que deseja realizar esta ação?').should('be.visible')
+    cy.contains('Atenção!').should('be.visible')
+
+    cy.wait(5000)
+  })
+
+  it('Ação em Lote: Pausar', function () {
     // Seleciona a aba "Todos"
     cy.contains('Todos').should('be.visible').click()
 
