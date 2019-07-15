0d185ead5085f17f9a26c64a41ea90b3bff56830
Guilherme Rios <guilherme.rc.98@gmail.com>
2019-07-15 18:47:10 -0300

    SAAS-536 Teste de mensagem do whatsapp

--- tests/e2e/specs/dispute/disputeSendWhats.spec.js
+++ tests/e2e/specs/dispute/disputeSendWhats.spec.js
@@ -1,7 +1,39 @@
 const login = Cypress.env('email1')
 const password = Cypress.env('password1')
+// const login2 = Cypress.env('')
+// const password = Cypress.env('')
 
 describe('Justto.App - Disputa: Menssagens', function () {
+  // it('Login', function () {
+  //   // Acessa a página inicial do Justto.App
+  //   // cy.visit('http://homol.justto.com.br')
+  //   cy.visit('localhost:8080')
+  //
+  //   // Redireciona para 'Login'
+  //   cy.url().should('include', '/#/login')
+  //
+  //   // Preenche o campo 'Email'
+  //   cy.get('[data-testid=login-email]')
+  //     .type(login)
+  //     .should('have.value', login)
+  //
+  //   // Preenche o campo 'Senha'
+  //   cy.get('[data-testid=login-password]')
+  //     .type(password)
+  //     .should('have.value', password)
+  //
+  //   // Clica no botão "Entrar"
+  //   cy.get('[data-testid=submit]')
+  //     .click()
+  //
+  //   // Verifica se tela acessada é a de "Gerenciamento"
+  //   cy.url().should('include', '/#/management')
+  // })
+  //
+  // it('Envio de Whatsapp: Não Sincronizado', function () {
+  //
+  // })
+
   it('Login', function () {
     // Acessa a página inicial do Justto.App
     // cy.visit('http://homol.justto.com.br')
@@ -28,9 +60,6 @@ describe('Justto.App - Disputa: Menssagens', function () {
     cy.url().should('include', '/#/management')
   })
 
-  // it('Envio de Whatsapp: Não Sincronizado', function () {
-  // })
-
   it('Envio de Whatsapp: Desconectado', function () {
     // Entra na primeira disputa da lista
     cy.wait(2000)
@@ -50,6 +79,4 @@ describe('Justto.App - Disputa: Menssagens', function () {
       .should('be.disabled')
   })
 
-  // it('Envio de Whatsapp: Sucesso', function () {
-  // })
 })
