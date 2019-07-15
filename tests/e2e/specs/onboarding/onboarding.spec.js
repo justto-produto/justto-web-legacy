7e71b3e414a2f226088c85d359081ebe160c5d10
jus-grazi <graziele@justto.com.br>
2019-07-15 15:06:49 -0300

    Variaveis de ambiente de Onboarding

--- tests/e2e/specs/onboarding/onboarding.spec.js
+++ tests/e2e/specs/onboarding/onboarding.spec.js
@@ -1,8 +1,8 @@
 describe('Justto.App - Onboarding', function () {
   beforeEach(function () {
     // Acessa a página inicial do Justto.App
-    // cy.visit('http://homol.justto.com.br')
-    cy.visit('localhost:8080')
+    cy.visit('https://kubernetes.justto.com.br')
+    // cy.visit('localhost:8080')
 
     // Sistema deve redirecionar para a página de Login
     cy.url().should('include', '/#/login')
