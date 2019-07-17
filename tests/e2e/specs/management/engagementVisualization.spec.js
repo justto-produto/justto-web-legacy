0017661d69825655ee38d5a10002cfbfb37c707c
Elize <elizete_srm@outlook.com>
2019-07-17 20:08:26 -0300

    Altera cy.visit de engagementVisualization

--- tests/e2e/specs/management/engagementVisualization.spec.js
+++ tests/e2e/specs/management/engagementVisualization.spec.js
@@ -4,7 +4,7 @@ const password = Cypress.env('password1')
 describe('Justto.App - Gerenciamento: Visualização Engajamento', function () {
   it('Gerenciamento: Engajamento - Exibição dos Casos - Vazio', function () {
     // Acessa a página inicial do Justto.App
-    cy.visit(Cypress.env('BASE_URL'))
+    cy.visit('/#/login')
 
     // Sistema deve redirecionar para a página de Login
     cy.url().should('include', '/#/login')
