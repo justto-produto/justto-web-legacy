8ab0fae0219a8472fb8d9681d1996c6008f2b4ac
Henrique Liberato <henriquebmx@gmail.com>
2019-07-24 16:07:07 -0300

    SJ-451 Corrige timezone vindo errado do elastic

--- src/plugins/moment.js
+++ src/plugins/moment.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueMoment from 'vue-moment'
 import moment from 'moment-timezone'
 import 'moment/locale/pt-br'
-moment.tz.setDefault('UTC')
 
 Vue.use(VueMoment, {
   moment
