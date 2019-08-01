eb6130e2ed33b82294262d9f682f58b75c99c9c8
Guilherme Rios <guilherme.rc.98@gmail.com>
2019-08-01 19:11:59 -0300

    SAAS-463 Aponta testes para kubernetes

--- tests/e2e/specs/dispute/disputeActionsLose.spec.js
+++ tests/e2e/specs/dispute/disputeActionsLose.spec.js
@@ -28,7 +28,7 @@ describe('Justto.App - Disputa: Ação Perder', function () {
 
     // Entra na disputa
     cy.get('[data-testid=dispute-index] tbody > tr.el-table__row').first()
-      .click(){force: true}
+      .click({force: true})
 
     // Sistema deve redirecionar para a página de Registro
     cy.url().should('include', '/#/management/dispute/')
