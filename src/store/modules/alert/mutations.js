b580690f671acc5eceda74635313bb691488c124
Mauricio Ribeiro <mauricio@justto.com.br>
2019-08-15 16:33:46 -0300

    SAAS-685 Implementa mutation de remoção de alertas

--- src/store/modules/alert/mutations.js
+++ src/store/modules/alert/mutations.js
@@ -6,6 +6,12 @@ const alertMutations = {
   },
   addAlert (state, alert) {
     state.alerts.push(alert)
+  },
+  removeAlert (state, alertChanged) {
+    Vue.nextTick(() => {
+      let alertIndex = state.alerts.findIndex(a => alertChanged.id === a.id)
+      Vue.delete(state.alerts, alertIndex)
+    })
   }
 }
 
