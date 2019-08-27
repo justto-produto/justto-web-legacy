ae084a0de4a9bf26b96dfb1af0fb2b52e7b1c610
Guilherme Rios <guilherme.rc.98@gmail.com>
2019-08-27 15:09:49 -0300

    SAAS-807 Teste de onboarding

--- tests/e2e/specs/onboarding/onboarding2.spec.js
+++ tests/e2e/specs/onboarding/onboarding2.spec.js
@@ -1,6 +1,6 @@
 describe('Justto.App - Onboarding', function () {
   const email = Cypress.env('onboarding-email')
-  const senha = Cypress.env('default-test-password')
+  const senha = Cypress.env('default-password')
 
   it('Onboarding: Login', function () {
     cy.visit('/#/login')
@@ -18,81 +18,79 @@ describe('Justto.App - Onboarding', function () {
   })
 
   it('Onboarding: Vamos começar', function () {
-    cy.get('[data-testid=welcome-next]')
+    cy.get('[data-testid=lets-start]')
       .click()
     cy.contains('Qual o nome da sua equipe?')
       .should('be.visible')
   })
 
   it('Onboarding: Nome da Equipe', function () {
-    cy.get('[data-testid=teamname-name]')
-      .type('Workspace de Teste')
-      .should('have.value', 'Workspace de Teste')
+    cy.get('[data-testid=teamname-form]')
+      .type('Teste Onboarding')
+      .should('have.value', 'Teste Onboarding')
     cy.get('[data-testid=teamname-next]')
       .click()
     cy.contains('Crie o seu espaço de trabalho na Justto')
     .should('be.visible')
   })
 
-  it('OnBoarding: Workspace Válido', function () {
+  const randomSubdomain = new Date().getTime().toString()
 
-    function randomSubdomain (range) {
-      var allLetters = '0123456789abcdefghiklmnopqrstuvwxyz'
-      var randomSubdomain = ''
-      for (var i = 0; i < range; i++) {
-        var randomNumber = Math.floor(Math.random() * allLetters.length)
-        randomSubdomain += allLetters.substring(randomNumber, randomNumber + 1)
-      }
-      var randomSubdomain = 'teste' + randomSubdomain
-      return randomSubdomain
-    }
+  // it('OnBoarding: Workspace em Formato Inválido', function () {
+  //   // Preenche campo de Workspace 'https://justto.app' com subdomínio 'teste-xxxxx'
+  //
+  //   cy.get('[data-testid=subdomain-form]')
+  //     .type(randomSubdomain + '@')
+  //     .should('have.value', randomSubdomain + '@')
+  //
+  //   // Seleciona Próximo
+  //   cy.get('[data-testid=subdomain-next]')
+  //     .click()
+  //
+  //   // Verifica se subdomínio digitado está disponível
+  //   cy.contains('Formato inválido')
+  //     .should('be.visible')
+  // })
+  //
+  // it('OnBoarding: Workspace Indisponivel', function () {
+  //   // Preenche campo de Workspace 'https://justto.app' com subdomínio 'teste-xxxxx'
+  //
+  //   cy.get('[data-testid=subdomain-form]')
+  //     .clear()
+  //     .type('simple')
+  //     .should('have.value', 'simple')
+  //
+  //   // Seleciona Próximo
+  //   cy.get('[data-testid=subdomain-next]')
+  //     .click()
+  //
+  //   // Verifica se subdomínio digitado está disponível
+  //   cy.contains('Este subdomínio já está em uso.')
+  //     .should('be.visible')
+  // })
 
+  it('OnBoarding: Workspace Sucesso', function () {
     // Preenche campo de Workspace 'https://justto.app' com subdomínio 'teste-xxxxx'
-    var subdomain = randomSubdomain(5)
-    cy.get('[data-testid=subdomain-name]')
-      .type(subdomain)
-    // .should(subdomain)
+
+    cy.get('[data-testid=subdomain-form]')
+      .clear()
+      .type(randomSubdomain)
+      .should('have.value', randomSubdomain)
+
 
     // Verifica se subdomínio digitado está disponível
     cy.contains('Subdomínio disponível.')
+      .should('be.visible')
 
     // Seleciona Próximo
     cy.get('[data-testid=subdomain-next]')
-      // .click()
+      .click()
 
     // Verifica próximo passo é 'Adicione pessoas à sua equipe'
-    cy.contains('Adicione pessoas à sua equipe').should('be.visible')
+    cy.contains('Adicione pessoas à sua equipe')
+      // .should('be.visible')
   })
 
-  //
-  // it('OnBoarding: Workspace Inválido', function () {
-  //   // Seleciona botão 'Vamos Começar'
-  //   cy.get('[data-testid=lets-start]')
-  //     .click()
-  //
-  //   // Verifica próximo passo é 'Qual o nome da sua equipe?'
-  //   cy.contains('Qual o nome da sua equipe?').should('be.visible')
-  //
-  //   // Preenche o campo 'Equipe'
-  //   cy.get('[data-testid=name-teamname]')
-  //     .type('Workspace de Teste')
-  //     .should('have.value', 'Workspace de Teste')
-  //
-  //   // Seleciona 'Próximo'
-  //   cy.get('[data-testid=next-teamname]')
-  //     .click()
-  //
-  //   // Verifica próximo passo é 'Crie o seu espaço de trabalho na Justto'
-  //   cy.contains('Crie o seu espaço de trabalho na Justto').should('be.visible')
-  //
-  //   // Preenche campo de Workspace 'https://justto.app' com subdomínio 'teste'
-  //   cy.get('[data-testid=name-subdomain]')
-  //     .type('teste')
-  //     .should('teste')
-  //
-  //   // Verifica se subdomínio digitado está indisponível
-  //   cy.contains('Este subdomínio já está em uso.').should('be.visible')
-  // })
   //
   // it('OnBoarding: Adicione Pessoas à sua Equipe, Válido', function () {
   //   // Seleciona botão 'Vamos Começar'
