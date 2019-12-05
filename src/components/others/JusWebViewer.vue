e6d32cadb0e5bb922a3f1fc64ba02c60ae9fc8af
Henrique Liberato <henriquebmx@gmail.com>
2019-12-05 19:08:48 -0300

    SAAS-1311 Implementa download e assinatura

--- src/components/others/JusWebViewer.vue
+++ src/components/others/JusWebViewer.vue
@@ -4,7 +4,6 @@
 
 <script>
 /* eslint-disable */
-
 export default {
   name: 'JusWebViewer',
   props: {
@@ -12,10 +11,29 @@ export default {
   },
   mounted: function () {
     WebViewer({
-      path: process.env.BASE_URL + 'lib',
-      initialDoc: this.url
+      path: process.env.BASE_URL + 'lib'
     }, this.$refs.viewer).then((instance) => {
-      // call apis here
+      instance.disableElements([
+        'searchButton',
+        'menuButton',
+        'toolsButton',
+        'eraserToolButton',
+        'outlinesPanel',
+        'outlinesPanelButton',
+        'viewControlsButton'
+      ])
+      instance.setMaxZoomLevel('150%')
+      instance.setMinZoomLevel('50%')
+      instance.disableAnnotations()
+      instance.disableNotesPanel()
+      instance.disableRedaction()
+      instance.disableTextSelection()
+      instance.loadDocument(this.url, {
+        customHeaders: {
+          Authorization: this.$store.getters.accountToken,
+          Workspace: this.$store.getters.workspaceSubdomain
+        }
+      })
     })
   }
 }
@@ -24,6 +42,6 @@ export default {
 <style scoped>
 div {
   width: 100%;
-  height: calc(100vh - 180px);
+  height: calc(100vh - 340px);
 }
 </style>
