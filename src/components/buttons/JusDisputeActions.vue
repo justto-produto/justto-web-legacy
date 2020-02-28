ac96159e957025e2568cf002469892b34b9011c3
Henrique Liberato <henriquebmx@gmail.com>
2020-02-28 16:52:39 -0300

    SAAS-1856 Corrige promise e loading

--- src/components/buttons/JusDisputeActions.vue
+++ src/components/buttons/JusDisputeActions.vue
@@ -409,62 +409,66 @@ export default {
       }
     },
     doAction (action) {
-      this.modalLoading = true
-      let translatedAction = this.$t('action.' + action.toUpperCase())
-      let params = {
-        action: action,
-        disputeId: this.dispute.id
-      }
-      if (this.unsettledType) {
-        params['body'] = { 'reason': this.unsettledTypes[this.unsettledType] }
-      }
-      if (action === 'settled' && this.settledValue) {
-        params.value = this.settledValue
-      }
-      if (this.tableActions && action !== 'unsettled') {
-        this.$jusNotification({
-          title: 'Yay!',
-          dangerouslyUseHTMLString: true,
-          message: `Ação <strong>${translatedAction}</strong> realizada com sucesso. Em instantes o sistema atualizará com as novas informações`,
-          type: 'success'
-        })
-      }
-      this.$store.dispatch('sendDisputeAction', params).then(() => {
-        // SEGMENT TRACK
-        this.$jusSegment(getTracktitleByAction(action), { disputeId: params.disputeId })
-        if (!this.tableActions || action === 'unsettled') {
+      return new Promise((resolve, reject) => {
+        this.modalLoading = true
+        let translatedAction = this.$t('action.' + action.toUpperCase())
+        let params = {
+          action: action,
+          disputeId: this.dispute.id
+        }
+        if (this.unsettledType) {
+          params['body'] = { 'reason': this.unsettledTypes[this.unsettledType] }
+        }
+        if (action === 'settled' && this.settledValue) {
+          params.value = this.settledValue
+        }
+        if (this.tableActions && action !== 'unsettled') {
           this.$jusNotification({
             title: 'Yay!',
             dangerouslyUseHTMLString: true,
-            message: `Ação <strong>${translatedAction}</strong> realizada com sucesso.`,
+            message: `Ação <strong>${translatedAction}</strong> realizada com sucesso. Em instantes o sistema atualizará com as novas informações`,
             type: 'success'
           })
         }
-        if (action === 'unsettled' && !this.tableActions) {
-          setTimeout(() => {
+        this.$store.dispatch('sendDisputeAction', params).then(() => {
+          // SEGMENT TRACK
+          resolve()
+          this.$jusSegment(getTracktitleByAction(action), { disputeId: params.disputeId })
+          if (!this.tableActions || action === 'unsettled') {
             this.$jusNotification({
-              title: 'Atenção!',
-              message: 'Enviaremos para às contrapartes uma mensagem de encerramento de negociação.',
-              type: 'info',
-              duration: 0
+              title: 'Yay!',
+              dangerouslyUseHTMLString: true,
+              message: `Ação <strong>${translatedAction}</strong> realizada com sucesso.`,
+              type: 'success'
             })
-          }, 2000)
-        }
-        if (!this.tableActions) this.$emit('fetch-data')
-      }).catch(e => {
-        console.error(e)
-        if (e.response && e.response.data.reason.length) {
-          this.$jusNotification({
-            type: 'error',
-            message: e.response.data.reason + '. Tente novamente ou entre em contato com o administrador do sistema.'
-          })
-        } else {
-          this.$jusNotification({ type: 'error' })
-        }
-      }).finally(() => {
-        this.chooseUnsettledDialogVisible = false
-        this.insertSettledValueDialogVisible = false
-        this.modalLoading = false
+          }
+          if (action === 'unsettled' && !this.tableActions) {
+            setTimeout(() => {
+              this.$jusNotification({
+                title: 'Atenção!',
+                message: 'Enviaremos para às contrapartes uma mensagem de encerramento de negociação.',
+                type: 'info',
+                duration: 0
+              })
+            }, 2000)
+          }
+          if (!this.tableActions) this.$emit('fetch-data')
+        }).catch(e => {
+          reject(e)
+          console.error(e)
+          if (e.response && e.response.data.reason.length) {
+            this.$jusNotification({
+              type: 'error',
+              message: e.response.data.reason + '. Tente novamente ou entre em contato com o administrador do sistema.'
+            })
+          } else {
+            this.$jusNotification({ type: 'error' })
+          }
+        }).finally(() => {
+          this.chooseUnsettledDialogVisible = false
+          this.insertSettledValueDialogVisible = false
+          this.modalLoading = false
+        })
       })
     },
     editNegotiators () {
@@ -497,12 +501,22 @@ export default {
     counterproposalDialogOpen () {
       if (this.dispute.paused) {
         this.$confirm('Esta disputa está pausada, deseja remotar?', 'Ops!', {
-          confirmButtonText: 'Continuar',
+          confirmButtonText: 'Retomar',
           cancelButtonText: 'Cancelar',
           type: 'warning',
-        }).then(() => {
-          this.doAction('resume')
-          this.openCounterproposalDialog()
+          beforeClose: (action, instance, done) => {
+            if (action === 'confirm') {
+              instance.confirmButtonLoading = true
+              this.doAction('resume').then(() => {
+                this.openCounterproposalDialog()
+              }).finally(() => {
+                done()
+                instance.confirmButtonLoading = false
+              })
+            } else {
+              done()
+            }
+          }
         })
       } else {
         this.openCounterproposalDialog()
