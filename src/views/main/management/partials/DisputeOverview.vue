4f06913216488273f1d3a798119d2bcfc64573a6
Jesus Justto <guilherme@jesus.com.br>
2020-02-28 17:05:24 -0300

    SAAS-1858 Altera estrutura JSON de namesake

--- src/views/main/management/partials/DisputeOverview.vue
+++ src/views/main/management/partials/DisputeOverview.vue
@@ -856,7 +856,8 @@ export default {
   },
   methods: {
     showNamesake (role) {
-      return role.personProperties.NAMESAKE && !role.documentNumber && role.party === 'CLAIMANT'
+
+      return role.namesake && !role.documentNumber && role.party === 'CLAIMANT'
     },
     closeNamesakes () {
       this.namesakeDialogVisible = false
