a788ce2212d937a00a4591555cb3932d6bce629d
Jesus Justto <guilherme@jesus.com.br>
2020-03-05 17:10:35 -0300

    SAAS-1892 Valida wrkspace periodicamente e em cada requisição

---
+++ src/utils/validateWorkspace.js
@@ -0,0 +1,14 @@
+import store from '@/store'
+
+const validateWorkspace = function () {
+  setInterval(function () {
+    let storageWorkspace = JSON.parse(localStorage.getItem('jusworkspace'))
+    if (store.getters.isLoggedIn && store.getters.hasWorkspace && storageWorkspace && storageWorkspace.subDomain) {
+      if (store.getters.workspaceSubdomain !== storageWorkspace.subDomain) {
+        window.location.reload()
+      }
+    }
+  }, 5000)
+}
+
+export { validateWorkspace }
