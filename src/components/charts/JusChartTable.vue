ecef68f8145ebd3bd8cd0574b4c2f496cbd42ccf
Henrique Liberato <henriquebmx@gmail.com>
2020-04-30 00:26:53 -0300

    SAAS-2195 Implementa click na tabela

--- src/components/charts/JusChartTable.vue
+++ src/components/charts/JusChartTable.vue
@@ -2,6 +2,7 @@
   <el-table
     :data="disputeStatusSummaryWithWarn"
     :header-row-class-name="headerRowClassName"
+    @cell-click="cellClick"
     max-height="100%"
     size="medium"
     show-summary
@@ -11,16 +12,19 @@
       class-name="status"
       label="Status"/>
     <el-table-column
+      :index="0"
       align="center"
       class-name="withoutAlert"
       prop="withoutAlert"
       label="Sem alerta"/>
     <el-table-column
+      :index="1"
       align="center"
       class-name="withAlert"
       prop="withAlert"
       label="Com alerta"/>
     <el-table-column
+      :index="2"
       align="center"
       prop="total"
       class-name="total"
@@ -39,21 +43,58 @@ export default {
   },
   computed: {
     disputeStatusSummaryWithWarn () {
+      let self = this
       let disputeStatusSummaryWithWarn = []
-      for (const label in this.data.labels) {
+      this.data.labels.forEach((label, index) => {
         disputeStatusSummaryWithWarn.push({
-          label: this.data.labels[label],
-          withoutAlert: this.data.datasets[0].data[label],
-          withAlert: this.data.datasets[1].data[label],
-          total: this.data.datasets[2].data[label]
+          label,
+          withoutAlert: self.data.datasets[0].data[index],
+          withAlert: self.data.datasets[1].data[index],
+          total: self.data.datasets[2].data[index],
+          statusIndex: index
         })
-      }
+      })
       return disputeStatusSummaryWithWarn
+    },
+    selectedMemberId () {
+      return this.$store.getters.dashboardSelectedMemberId
     }
   },
   methods: {
     headerRowClassName ({ row, rowIndex }) {
       return 'header'
+    },
+    cellClick (row, column, cell, event) {
+      let statusIndex = row.statusIndex
+      let cellIndex = cell.cellIndex
+      let columnIndex = column.index
+      let filters = this.data.filter[statusIndex]
+      let datasetFilters = this.data.datasets[columnIndex] ? this.data.datasets[columnIndex].filter[cellIndex] : null
+      if (datasetFilters) {
+        const filters = Object.assign(filters, datasetFilters)
+        this.$confirm(
+          'Deseja ver as disputas no painel de gerenciamento?',
+          'Ir para gerenciamento', {
+            confirmButtonText: 'Continuar',
+            cancelButtonText: 'Cancelar',
+            cancelButtonClass: 'is-plain',
+            type: 'info'
+          }).then(() => {
+          this.$store.commit('clearDisputeQuery')
+          this.$store.commit('updateDisputeQuery', { key: 'status', value: [] })
+          for (let key in filters) {
+            if (filters.hasOwnProperty(key)) {
+              this.$store.commit('updateDisputeQuery', { key, value: filters[key] })
+            }
+          }
+          if (this.selectedMemberId) {
+            this.$store.commit('updateDisputeQuery', { key: 'persons', value: [this.selectedMemberId] })
+          }
+          this.$store.commit('setDisputeHasFilters', true)
+          this.$store.commit('setDisputesTab', '3')
+          this.$router.push('/management')
+        })
+      }
     }
   }
 }
