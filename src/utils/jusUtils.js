6c15ba4abdfec83f719a11407e7492689cbafdd7
Henrique Liberato <henriquebmx@gmail.com>
2020-05-08 16:32:16 -0300

    SAAS-2144 Corrige get em tags

--- src/utils/jusUtils.js
+++ src/utils/jusUtils.js
@@ -1,6 +1,35 @@
 import Fuse from 'fuse.js'
 import moment from 'moment'
 
+const queryBuilder = (q, command, disputesLength, noSort) => {
+  let query = '?'
+  for (let [key, value] of Object.entries(q)) {
+    if (['total'].includes(key)) continue
+    if (!value) continue
+    if (Array.isArray(value)) {
+      if (!value.length) continue
+      if (['expirationDate', 'dealDate', 'importingDate'].includes(key)) {
+        let startDate = moment(value[0]).startOf('day').utc().format('YYYY-MM-DD[T]HH:mm:ss[Z]')
+        let endDate = moment(value[1]).endOf('day').utc().format('YYYY-MM-DD[T]HH:mm:ss[Z]')
+        query = `${query}${key}Start=${startDate}&${key}End=${endDate}&`
+      } else {
+        for (let v of value) {
+          query = query + key + '=' + v + '&'
+        }
+      }
+    } else if (noSort) {
+      continue
+    } else if (key === 'page') {
+      query = query + key + '=' + ((command === 'update' ? 1 : value) - 1) + '&'
+    } else if (key === 'size') {
+      query = query + key + '=' + (command === 'update' ? disputesLength : value) + '&'
+    } else {
+      query = query + key + '=' + value + '&'
+    }
+  }
+  return query
+}
+
 const buildRoleTitle = function (party, title) {
   if (party === 'RESPONDENT') {
     switch (title) {
@@ -258,5 +287,6 @@ export {
   isBase64,
   uuidv4,
   getDocumentStep,
-  getTracktitleByAction
+  getTracktitleByAction,
+  queryBuilder
 }
