b78aa7346052656d85555a9020a12ccb658ffd3e
Jesus Justto <guilherme@jesus.com.br>
2020-05-12 17:22:30 -0300

    SAAS-2241 Implementa axiosDispatcher e jsdocs

--- src/store/modules/document/actions.js
+++ src/store/modules/document/actions.js
@@ -2,29 +2,22 @@ import axiosDispatcher from '@/store/axiosDispatcher.js'
 const FileSaver = require('file-saver')
 const documents = 'api/office/documents/'
 
+/**
+ * Dispatch Actions
+ * @type {Promise}
+ * @param {string} url - the end-point reqeust
+ * @param {string} method - the method of requesr (default is 'GET')
+ * @param {Object, array} data - the params  of request
+ */
+
 const actions = {
   getDocumentModels ({ commit }) {
-    return new Promise((resolve, reject) => {
-      // eslint-disable-next-line
-      axios.get(documents + 'model')
-        .then(response => {
-          resolve(response.data)
-        })
-        .catch(error => {
-          reject(error)
-        })
-    })
+    return axiosDispatcher({ url: documents + 'model' })
   },
   createDocumentByModel ({ commit }, params) {
-    return new Promise((resolve, reject) => {
-      // eslint-disable-next-line
-      axios.post(documents + params.modelId + '/' + params.disputeId)
-        .then(response => {
-          resolve(response.data)
-        })
-        .catch(error => {
-          reject(error)
-        })
+    return axiosDispatcher({
+      url: documents + params.modelId + '/' + params.disputeId,
+      method: 'POST'
     })
   },
   getDocumentByDisputeId ({ commit }, disputeId) {
@@ -42,43 +35,29 @@ const actions = {
     })
   },
   setDocumentSigners ({ commit }, params) {
-    return new Promise((resolve, reject) => {
-      // eslint-disable-next-line
-      axios.post(documents + 'signer/' + params.disputeId, params.recipients).then(response => {
-        resolve(response.data)
-      }).catch(error => {
-        reject(error)
-      })
+    return axiosDispatcher({
+      url: documents + 'signer/' + params.disputeId,
+      method: 'POST',
+      data: params.recipients
     })
   },
   resendSignersNotification ({ commit }, params) {
-    return new Promise((resolve, reject) => {
-      // eslint-disable-next-line
-      axios.put(documents + 'resend-notification/' + params.disputeId).then(response => {
-        resolve(response.data)
-      }).catch(error => {
-        reject(error)
-      })
+    return axiosDispatcher({
+      url: documents + 'resend-notification/' + params.disputeId,
+      method: 'PUT'
     })
   },
   deleteDocument ({ commit }, disputeId) {
-    return new Promise((resolve, reject) => {
-      // eslint-disable-next-line
-      axios.delete(documents + disputeId).then(response => {
-        resolve(response)
-      }).catch(error => {
-        reject(error)
-      })
+    return axiosDispatcher({
+      url: documents + disputeId,
+      method: 'DELETE'
     })
   },
   addModel ({ commit }, url) {
-    return new Promise((resolve, reject) => {
-      // eslint-disable-next-line
-      axios.post(documents + `model?url=${url}`,  {}).then(response => {
-        resolve(response)
-      }).catch(error => {
-        reject(error)
-      })
+    return axiosDispatcher({
+      url: documents + `model?url=${url}`,
+      method: 'POST',
+      data: {}
     })
   },
   editModel ({ commit }, model) {
@@ -89,13 +68,9 @@ const actions = {
     })
   },
   deleteModel ({ commit }, modelId) {
-    return new Promise((resolve, reject) => {
-      // eslint-disable-next-line
-      axios.delete(documents + 'model/' + modelId).then(response => {
-        resolve(response)
-      }).catch(error => {
-        reject(error)
-      })
+    return axiosDispatcher({
+      url: documents + 'model/' + modelId,
+      method: 'DELETE'
     })
   },
   downloadDocument ({ commit }, params) {
