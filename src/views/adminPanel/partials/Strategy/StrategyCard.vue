f8820425f3164ea90a57c64d02edca484ea15a64
srvaz <jonwify@gmail.com>
2020-06-05 08:50:50 -0300

    :sparkles: Adiciona StrategyCard

---
+++ src/views/adminPanel/partials/Strategy/StrategyCard.vue
@@ -0,0 +1,96 @@
+<template>
+  <el-card class="strategy-card">
+    <div class="strategy-card__icon-area">
+      <i
+        :class="{
+          [`el-icon-${strategyData.isPrivate ? 'lock' : 'unlock'}`]: true
+      }"/>
+    </div>
+
+    <div class="strategy-card__name-area">
+      <jus-text-editable
+        :value="strategyData.name"
+        type="title"
+        @hasEdition="changeEstrategyData"
+      />
+    </div>
+    <div class="strategy-card__workspaces-area"/>
+    <div class="strategy-card__messages-area"/>
+    <div class="strategy-card__strategies-area"/>
+  </el-card>
+</template>
+
+<script>
+import { JusTextEditable } from '@/components/JusTextEditable'
+
+export default {
+  name: 'PanelStrategy',
+  components: {
+    JusTextEditable,
+  },
+  props: {
+    strategy: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      strategyData: this.strategy,
+    }
+  },
+  methods: {
+    changeEstrategyData(val) {
+      this.strategyData.name = val
+      this.$emit('changeEstrategyData', this.strategyData)
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+@import '@/styles/nestable.scss';
+
+.panel-strategy {
+  padding: 40px;
+
+  .strategy-card {
+    & > .el-card__body {
+      display: grid;
+      grid-template-areas:
+        'icon-area name-area name-area name-area'
+        'icon-area workspaces-area messages-area strategies-area';
+      grid-template-columns: 32px repeat(3, 1fr);
+      grid-template-rows: repeat(2, minmax(40px, auto));
+      gap: 16px;
+
+      .strategy-card__icon-area {
+        grid-area: icon-area;
+
+        & > i {
+          font-size: 32px;
+        }
+      }
+
+      .strategy-card__name-area {
+        grid-area: name-area;
+      }
+
+      .strategy-card__workspaces-area {
+        grid-area: workspaces-area;
+        background: slateblue;
+      }
+
+      .strategy-card__messages-area {
+        grid-area: messages-area;
+        background: gold;
+      }
+
+      .strategy-card__strategies-area {
+        grid-area: strategies-area;
+        background: aquamarine;
+      }
+    }
+  }
+}
+</style>
