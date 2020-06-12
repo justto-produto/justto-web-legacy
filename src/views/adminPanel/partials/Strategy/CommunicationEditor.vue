06781527725ac74d38ac4f51d20db416a270f13d
srvaz <jonwify@gmail.com>
2020-06-12 14:50:09 -0300

    :sparkles: Adiciona variáveis na menssagem
    SAAS-2331

--- src/views/adminPanel/partials/Strategy/CommunicationEditor.vue
+++ src/views/adminPanel/partials/Strategy/CommunicationEditor.vue
@@ -3,6 +3,7 @@
     v-if="visible"
     :visible.sync="visible"
     :title="communicationData.name"
+    width="750px"
   >
     <div class="communication-editor">
       <quill-editor
@@ -12,6 +13,8 @@
         class="communication-editor__quill"
       />
     </div>
+
+    <JusVariablesCard :variables="variables" />
   </el-dialog>
 </template>
 
@@ -22,6 +25,7 @@ export default {
   name: 'CommunicationEditor',
   components: {
     quillEditor,
+    JusVariablesCard: () => import('@/components/layouts/JusVariablesCard'),
   },
   props: {
     communicationData: {
@@ -47,6 +51,10 @@ export default {
           ],
         },
       },
+      variables: {
+        teste1: 'teste 1',
+        teste2: 'teste 2',
+      },
     }
   },
   computed: {
@@ -61,11 +69,17 @@ export default {
 .communication-editor {
   border: 1px solid #ccc;
   border-radius: 4px;
-  display: grid;
-  grid-template-columns: 1fr 300px;
 
   .communication-editor__quill {
     padding: 16px;
   }
+}
+</style>
+
+<style lang="scss">
+.el-dialog__body {
+  display: grid;
+  gap: 24px;
+  grid-template-columns: 1fr auto;
 }
 </style>
