32d37f378e2eb06d87570e4beb305acd578f1f90
William Vitorino <josewilliam@justto.com.br>
2020-08-10 20:13:14 -0300

    SAAS-2587 :art: Refatora chamadas do vuex e adiciona validação da aba de financeiro.
    Signed-off-by: William Vitorino <josewilliam@justto.com.br>

--- src/views/main/MainContainer.vue
+++ src/views/main/MainContainer.vue
@@ -56,7 +56,7 @@
           <span slot="title">Importação</span>
         </el-menu-item>
         <el-menu-item
-          v-if="$store.getters.isJusttoAdmin"
+          v-if="isJusttoAdmin || isAdminProfile"
           index="/billing"
           data-testid="menu-financial"
         >
@@ -68,7 +68,7 @@
         </el-menu-item>
       </el-menu>
       <div
-        v-show="$store.getters.workspaceMembers.length && $store.getters.isAdminProfile"
+        v-show="workspaceMembers.length && isAdminProfile"
         class="jus-team-menu__title"
       >
         TIME
@@ -89,6 +89,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'MainContainer',
   components: {
@@ -103,6 +104,11 @@ export default {
     }
   },
   computed: {
+    ...mapGetters([
+      'isJusttoAdmin',
+      'isAdminProfile',
+      'workspaceMembers',
+    ]),
     workspace() {
       return this.$store.getters.workspaceSubdomain
     },
