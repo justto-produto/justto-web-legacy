ffaa8cca4728df78505f9f9827458f2894248e40
William Vitorino <josewilliam@justto.com.br>
2020-08-19 11:33:37 -0300

    SAAS-2636 :clown_face: Adiciona componente de Timeline.

---
+++ src/components/JusTimeline/JusTimeline.vue
@@ -0,0 +1,37 @@
+<template>
+  <section
+    id="jus-timeline"
+    class="el-container is-vertical">
+    <div class="block">
+      <el-timeline>
+        <el-timeline-item
+          v-for="(date, indexDate) in Object.keys(disputeProgress)"
+          :key="indexDate"
+          placement="top"
+          :timestamp="date">
+          <el-card>
+            {{ disputeProgress[date].title }}
+          </el-card>
+        </el-timeline-item>
+      </el-timeline>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      disputeProgress: {
+        '19/08/2020': { title: 'Titulo aqui...' },
+        '18/08/2020': { title: 'Titulo aqui...' },
+        '17/08/2020': { title: 'Titulo aqui...' },
+      },
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
