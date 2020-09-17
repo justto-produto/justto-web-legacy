e8254d5af0733fd764fee2252ac0172508dcdf3d
Gabriel Debona <gabriel@justto.com.br>
2020-09-17 18:02:27 -0300

    SAAS-2806 Melhora a função getStringInitials

--- src/utils/jusUtils.js
+++ src/utils/jusUtils.js
@@ -277,7 +277,15 @@ const filterByTerm = function(term, array, key1, key2) {
 
 const isJusttoUser = (email = '') => email.endsWith('@justto.com.br')
 
-const getStringInitials = (string) => string.substring(0, 2).toLowerCase()
+const getStringInitials = (string) => {
+  const stringSplited = string.split(' ').filter(Boolean)
+  if (stringSplited.length >= 2) {
+    return stringSplited[0].substring(0, 1).toLowerCase() +
+      stringSplited[stringSplited.length - 1].substring(0, 1).toLowerCase()
+  } else {
+    return string.substring(0, 2).toLowerCase()
+  }
+}
 
 export {
   buildRoleTitle,
