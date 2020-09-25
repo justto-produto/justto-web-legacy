99ed22a4187748e623b131b74de578aca9a2947a
Gabriel Debona <gabriel@justto.com.br>
2020-09-25 10:17:25 -0300

    SAAS-2835 Altera o texto do tour

--- src/views/main/dashboard/tour.js
+++ src/views/main/dashboard/tour.js
@@ -3,8 +3,8 @@ export default
   name: 'DASHBOARD_TOUR',
   steps: [
     {
-      title: 'Pré-negociação',
-      description: 'Opa, legal. Você importou um monte de processos para negociar, mas identificamos que pode ter ocorrido baixa definitiva em alguns…',
+      title: 'Gerenciamento',
+      description: 'Aqui neste botão, você acessa o gerenciamento de todas as disputas',
       asset: '',
       target: '[data-jus-tour="MANAGEMENT_MENU"]',
       steps: 0,
