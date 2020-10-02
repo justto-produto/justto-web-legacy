3f0d698adcdadbf8f3f773bb6cdab848e99db525
Zé William <josewilliam@justto.com.br>
2020-10-02 16:40:09 -0300

    🐛 Ajusta validação de inclusão da aba "Data do acordo"

--- src/views/main/management/partials/ManagementTable.vue
+++ src/views/main/management/partials/ManagementTable.vue
@@ -222,7 +222,7 @@
         </template>
       </el-table-column>
       <el-table-column
-        v-if="tab0 || tab2 || tab3"
+        v-if="ttab2 || tab3 || tabAll"
         :sortable="false"
         prop="disputeDealDate"
         label="Data do acordo"
