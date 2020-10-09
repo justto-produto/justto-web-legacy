523c15effa4793c1c107b1597284a435a4b3eaa2
Zé William <josewilliam@justto.com.br>
2020-10-09 15:45:16 -0300

    SAAS-2899 ✨ Adiciona componente de edição da template d respostas rápidas.

---
+++ src/views/main/management/partials/DisuteQuickReplyEditor.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="reply-editor__container">
+    <div class="reply-editor__body">
+      <el-input v-model="state.title" />
+      <ckeditor
+        v-if="state.body"
+        ref="templateEditor"
+        v-model="state.body"
+        class="reply-editor__editor"
+        :config="editorConfig"
+      />
+    </div>
+    <div class="reply-editor__footer">
+      <el-row
+        class="reply-editor__footer-row"
+        type="flex"
+        justify="end"
+        :gutter="20"
+      >
+        <el-col
+          :span="10"
+          class="reply-editor__footer-column"
+        >
+          <el-button @click="cancel()">
+            Cancelar
+          </el-button>
+          <el-button
+            type="primary"
+            :disabled="loading"
+            @click="save()"
+          >
+            Salvar
+          </el-button>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+import CKEditor from 'ckeditor4-vue'
+
+export default {
+  components: {
+    ckeditor: CKEditor.component
+  },
+  props: {
+    template: {
+      type: Object,
+      required: true
+    },
+    disputeId: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      html: '',
+      editorConfig: {
+        toolbarGroups: [
+          { name: 'document', groups: ['mode', 'document', 'doctools'] },
+          { name: 'clipboard', groups: ['clipboard', 'undo'] },
+          { name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing'] },
+          { name: 'forms', groups: ['forms'] },
+          { name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
+          { name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph'] },
+          { name: 'links', groups: ['links'] },
+          { name: 'insert', groups: ['insert'] },
+          { name: 'styles', groups: ['styles'] },
+          { name: 'colors', groups: ['colors'] },
+          { name: 'tools', groups: ['tools'] },
+          { name: 'others', groups: ['others'] },
+          { name: 'about', groups: ['about'] }
+        ],
+        removeButtons: 'Strike,Subscript,Superscript,CopyFormatting,Outdent,Indent,CreateDiv,Blockquote,BidiLtr,BidiRtl,Language,Link,Unlink,Anchor,Flash,Image,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,FontSize,Font,Format,Styles,TextColor,BGColor,ShowBlocks,Maximize,About,HiddenField,ImageButton,Button,Select,Textarea,TextField,Form,Scayt,SelectAll,Find,Replace,Redo,Undo,Cut,Copy,Paste,PasteText,PasteFromWord,Print,Preview,ExportPdf,NewPage,Templates,Save',
+        removePlugins: 'elementspath,resize'
+      },
+      state: { title: '', body: '' }
+    }
+  },
+  computed: {
+    ...mapGetters(['loading'])
+  },
+  watch: {
+    template() {
+      this.state = { ...this.template, body: this.formatBody() }
+    }
+  },
+  mounted() {
+    this.state = { ...this.template, body: this.formatBody() }
+  },
+  beforeDestroy() {
+    this.destroyEditor()
+    this.state = { title: '', body: '' }
+  },
+  methods: {
+    ...mapActions([
+      'editTemplate',
+      'showLoading',
+      'hideLoading'
+    ]),
+
+    destroyEditor() {
+      for (const instance of Object.values(window.CKEDITOR.instances)) {
+        instance.destroy()
+      }
+    },
+
+    cancel() {
+      this.destroyEditor()
+      this.$emit('cancel')
+      this.hideLoading()
+    },
+
+    save() {
+      this.showLoading()
+      this.editTemplate({
+        template: this.state,
+        disputeId: this.disputeId
+      }).then(res => {
+        this.destroyEditor()
+        this.$emit('input', res)
+        this.$emit('update')
+      }).catch(error => {
+        this.$jusNotification({ error })
+      }).finally(this.cancel)
+    },
+
+    formatBody() {
+      const { body } = this.template
+      if (body) {
+        const start = body.indexOf('<body>') + 6
+        const end = body.indexOf('</body>') - 7
+        if (start > 5 && end > 0) {
+          return body.substring(start, end).trim()
+        }
+      }
+      return body
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.reply-editor__container {
+  margin-top: 16px;
+  padding-top: 16px;
+
+  .reply-editor__body {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 16px;
+  }
+
+  .reply-editor__footer {
+    margin-top: 16px;
+
+    .reply-editor__footer-row {
+      .reply-editor__footer-column {
+        width: 100%;
+        display: flex;
+        justify-content: flex-end;
+      }
+    }
+  }
+}
+</style>
