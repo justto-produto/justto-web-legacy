1cfa7f399b384ff3ee231c6e91303361d0cc8a4d
Zé William <josewilliam@justto.com.br>
2020-11-23 20:42:39 -0300

    🐛 Corrige flag de emviar mensagem em horario comercial.

--- src/views/main/dispute/partials/DisputeOverview/DisputeOverview.vue
+++ src/views/main/dispute/partials/DisputeOverview/DisputeOverview.vue
@@ -278,11 +278,11 @@
               <span class="title">Configurações:</span>
               <span class="configurations">
                 Enriquecer automaticamente na importação?
-                <div><i :class="!dispute.skipEnrichment ? 'el-icon-check' : 'el-icon-close'" /> {{ !dispute.skipEnrichment ? 'Sim' : 'Não ' }}</div>
+                <div><i :class="dispute.skipEnrichment ? 'el-icon-close' : 'el-icon-check'" /> {{ dispute.skipEnrichment ? 'Não' : 'Sim' }}</div>
                 Somente depósito em conta-corrente?
                 <div><i :class="dispute.denySavingDeposit ? 'el-icon-check' : 'el-icon-close'" /> {{ dispute.denySavingDeposit ? 'Sim' : 'Não ' }}</div>
                 Mensagens somente em horário comercial?
-                <div><i :class="dispute.businessHoursEngagement ? 'el-icon-close' : 'el-icon-check'" /> {{ dispute.businessHoursEngagement ? 'Não' : 'Sim' }}</div>
+                <div><i :class="dispute.businessHoursEngagement ? 'el-icon-check' : 'el-icon-close'" /> {{ dispute.businessHoursEngagement ? 'Sim' : 'Não' }}</div>
                 Contactar autor?
                 <div>
                   <i :class="(dispute.contactPartyWhenNoLowyer || dispute.contactPartyWhenInvalidLowyer) ? 'el-icon-check' : 'el-icon-close'" />
