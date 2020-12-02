3c3d1af252ef76247cc53dfa3c214278fd60d048
Zé William <josewilliam@justto.com.br>
2020-12-02 11:46:16 -0300

    🐛 Corrige roteamento ao trocar de Aba.

--- src/components/layouts/JusHeaderMain.vue
+++ src/components/layouts/JusHeaderMain.vue
@@ -226,13 +226,15 @@ export default {
       this.$store.dispatch('getWorkspaceMembers')
         .then(() => {
           this.$jusSegment('Troca de time/workspace', { description: `Alterado de ${workspace.workspace.name} para ${oldWorkspace}` })
-          this.$store.commit('setDisputesTab', '2')
-          this.$router.go('/management')
-          this.changeWorkspaceDialogVisible = true
+          // this.$store.commit('setDisputesTab', '2')
+          if (this.$route.params && this.$route.params.id) {
+            this.$router.push('/management')
+          }
           this.setPersonName()
         }).catch(error => {
           this.$jusNotification({ error })
         }).finally(() => {
+          this.changeWorkspaceDialogVisible = false
           setTimeout(() => {
             loading.close()
           }, 1000)
