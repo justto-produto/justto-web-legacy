efc690f17fa581072aa2b43f41ccccee8c9467bf
Zé William <josewilliam@justto.com.br>
2021-01-13 20:00:49 -0300

    💄 Estilo padrão do Ticket

--- src/views/main/negotiation/partials/Ticket.vue
+++ src/views/main/negotiation/partials/Ticket.vue
@@ -1,10 +1,12 @@
 <template>
-  <section>
-    <header />
-    <article>
+  <section
+    id="ticket-negotiation-container"
+    class="ticket-negotiation">
+    <header class="ticket-negotiation__header" />
+    <article class="ticket-negotiation__omnichanel">
       <Omnichannel />
     </article>
-    <aside>
+    <aside class="ticket-negotiation__overview">
       <Overview />
     </aside>
   </section>
@@ -20,6 +22,34 @@ export default {
 }
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+.ticket-negotiation {
+  display: flex;
+  // flex-direction: column;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  justify-content: space-between;
 
+  .ticket-negotiation__header {
+    justify-self: flex-start;
+    height: 78px;
+    background-color: white;
+
+    @media (max-height: 680px) {
+      height: 60px;
+    }
+  }
+
+  .ticket-negotiation__omnichanel {
+    justify-self: flex-end;
+    height: 100%;
+  }
+
+  .ticket-negotiation__overview {
+    height: 30vh;
+    justify-self: flex-end;
+  }
+
+}
 </style>
