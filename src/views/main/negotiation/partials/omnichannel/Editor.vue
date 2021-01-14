16cb72ade0a30c566456d89fa7e7e8710c6e699c
Zé William <josewilliam@justto.com.br>
2021-01-14 19:04:24 -0300

    SAAS-3283 🚧 Implementação básica do editor.

--- src/views/main/negotiation/partials/omnichannel/Editor.vue
+++ src/views/main/negotiation/partials/omnichannel/Editor.vue
@@ -1,18 +1,103 @@
 <template>
   <section
-    id="editorOmnichannelNEgotiation"
+    id="editorOmnichannelNegotiation"
     class="editor-container"
   >
-    EDITOR AQUI
+    <el-tabs
+      class="editor-container__tabs"
+      :value="activeTab"
+      @tab-click="setTab"
+    >
+      <el-tab-pane
+        v-for="(tab, tabIndex) in tabs"
+        :key="tabIndex"
+        :name="tab.name"
+        :label="tab.label">
+        <component
+          :is="tab.component.name"
+          v-if="tab.component"
+        />
+      </el-tab-pane>
+    </el-tabs>
   </section>
 </template>
 
 <script>
-export default {
+import { mapActions, mapGetters } from 'vuex'
+import EDITOR_CONSTANTS from '@/constants/editor'
 
+export default {
+  components: {
+    messages: () => import('./partials/Messages'),
+    notes: () => import('./partials/Notes')
+  },
+  props: {
+    size: {
+      type: String,
+      default: 'NORMAL'
+    }
+  },
+  computed: {
+    ...mapGetters({
+      activeTab: 'getActiveTab'
+    }),
+
+    height: {
+      get() {
+        return this.size
+      },
+      set(size) {
+        this.$emit('update:size', size)
+      }
+    },
+
+    tabs() {
+      return [
+        {
+          name: EDITOR_CONSTANTS.MESSAGES,
+          label: this.$tc('negotiation.ticket.omnichannel.editor.tab.message', 1),
+          component: {
+            name: 'messages',
+            props: {}
+          }
+        },
+        {
+          name: EDITOR_CONSTANTS.NOTES,
+          label: this.$tc('negotiation.ticket.omnichannel.editor.tab.note', 1),
+          component: {
+            name: 'notes',
+            props: {}
+          }
+        },
+        {
+          name: EDITOR_CONSTANTS.OCCURRENCES,
+          label: this.$tc('negotiation.ticket.omnichannel.editor.tab.occurrence', 1),
+          component: null
+        }
+      ]
+    }
+  },
+  methods: {
+    ...mapActions([
+      'setActiveTab'
+    ]),
+    setTab(tab, _event) {
+      this.setActiveTab(tab.name)
+    }
+  }
 }
 </script>
 
+<style lang="scss">
+.el-tabs__header {
+  margin-bottom: 0px !important;
+}
+</style>
+
 <style lang="scss" scoped>
 
+.editor-container {
+  background-color: transparent;
+  height: 100vh;
+}
 </style>
