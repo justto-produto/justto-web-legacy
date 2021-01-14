0eef5bed9bc0d0c99837fb02271207be582d83b6
Zé William <josewilliam@justto.com.br>
2021-01-14 19:06:20 -0300

    SAAS-3283 🚧 Innit do componente de Notas.

---
+++ src/views/main/negotiation/partials/omnichannel/partials/Notes.vue
@@ -0,0 +1,19 @@
+<template>
+  <section
+    id="notesTabEditorOmnichannelNegotiation"
+    class="notes-container">
+    Notes
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.notes-container {
+  background-color: transparent;
+}
+</style>
