98c7fecaed18984abc8af18a8d0661e381c8a6e7
Zé William <josewilliam@justto.com.br>
2021-01-14 10:06:59 -0300

    SAAS-3283 🚧 Cria componente do editor.

---
+++ src/views/main/negotiation/partials/omnichannel/Editor.vue
@@ -0,0 +1,18 @@
+<template>
+  <section
+    id="editorOmnichannelNEgotiation"
+    class="editor-container"
+  >
+    EDITOR AQUI
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
