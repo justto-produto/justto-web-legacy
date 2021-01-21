3288f3215e684314d54115aa0131415fe8286a3f
Zé William <josewilliam@justto.com.br>
2021-01-21 13:20:29 -0300

    SAAS-3313 ✨ Implementa menu de usuário no Overview do Ticket.

--- src/views/main/negotiation/partials/overview/Overview.vue
+++ src/views/main/negotiation/partials/overview/Overview.vue
@@ -3,21 +3,37 @@
     id="overviewOmnichanelNegotiatorContainer"
     class="overview-container"
   >
+    <header-user-menu class="overview-container__header" />
     OVERVIEW VAI SER AQUI
   </section>
 </template>
 
 <script>
 export default {
-  name: 'Overview'
+  name: 'Overview',
+  components: {
+    HeaderUserMenu: () => import('@/components/menus/HeaderUserMenu')
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .overview-container {
+  padding: 6px;
   background-color: transparent;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: 60px calc(100vh - 60px);
+
+  @media (max-height: 680px) {
+    height: 40px;
+    grid-template-rows: 40px calc(100vh - 40px);
+  }
+
+  &__header {
+    display: flex;
+    justify-self: center;
+    align-items: center;
+  }
 }
 </style>
