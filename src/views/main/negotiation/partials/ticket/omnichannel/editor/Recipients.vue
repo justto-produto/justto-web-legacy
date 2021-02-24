cb85acd028b80d44b237ea9a392ac62552b26c67
Zé William <josewilliam@justto.com.br>
2021-02-24 18:41:13 -0300

    SAAS-3448 🚧 Adiciona indicador do tipo de endereço.

--- src/views/main/negotiation/partials/ticket/omnichannel/editor/Recipients.vue
+++ src/views/main/negotiation/partials/ticket/omnichannel/editor/Recipients.vue
@@ -3,10 +3,19 @@
     id="recipientsMessagesEditorOmnichannelTicketNegotiation"
     class="recipients-container"
   >
-    <span v-if="recipients.length > 0">
-      Destinatários:
-      {{ recipients[0].address }}
-      {{ recipients.length > 1 ? `+${recipients.length - 1}` : '' }}
+    <span
+      v-if="recipients.length > 0"
+      class="recipients-container_content">
+      <span class="recipients-container_label">
+        Destinatários:
+        {{ recipients[0].address }}
+        {{ recipients.length > 1 ? `+${recipients.length - 1}` : '' }}
+      </span>
+      <jus-icon
+        :icon="type"
+        :is-active="!!type && type !== 'negotiation'"
+        class="recipients-container_icon"
+        :class="type" />
     </span>
   </section>
 </template>
@@ -17,7 +26,8 @@ export default {
   name: 'Recipients',
   computed: {
     ...mapGetters({
-      recipients: 'getEditorRecipients'
+      recipients: 'getEditorRecipients',
+      type: 'getEditorMessageType'
     })
   }
 }
@@ -28,5 +38,20 @@ export default {
   height: 48px;
   display: flex;
   align-items: center;
+
+  .recipients-container_content {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    gap: 6px;
+
+    .recipients-container_icon {
+      width: 16px;
+
+      &.negotiation {
+        margin-top: 2px;
+      }
+    }
+  }
 }
 </style>
