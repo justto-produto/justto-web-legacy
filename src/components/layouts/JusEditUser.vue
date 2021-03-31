cc0074f0bc0b4d970ac878b456e7a49ce35f8a8a
Zé William <josewilliam@justto.com.br>
2021-03-31 13:01:15 -0300

    SAAS-3552 🚧 Tela de editar dados pessoais.

---
+++ src/components/layouts/JusEditUser.vue
@@ -0,0 +1,58 @@
+<template>
+  <article class="edit-user-container">
+    <el-form :model="form">
+      <el-form-item
+        label="Nome"
+        prop="name"
+      >
+        <el-input
+          v-model="form.name"
+        >
+          <el-button slot="append">
+            Alterar
+          </el-button>
+        </el-input>
+      </el-form-item>
+
+      <el-form-item
+        label="Alterar Senha"
+        prop="password"
+      >
+        <el-input
+          v-model="form.password"
+          type="password"
+          autocomplete="false"
+          readonly
+          onfocus="this.removeAttribute('readonly');"
+        >
+          <el-button slot="append">
+            Alterar
+          </el-button>
+        </el-input>
+      </el-form-item>
+
+      <el-form-item
+        label="Telefone de contato"
+        prop="phone"
+      >
+        <el-input v-model="form.phone">
+          <el-button slot="append">
+            Alterar
+          </el-button>
+        </el-input>
+      </el-form-item>
+    </el-form>
+  </article>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    form: {
+      name: '',
+      password: '',
+      phone: ''
+    }
+  })
+}
+</script>
