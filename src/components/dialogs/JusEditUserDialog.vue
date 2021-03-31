b2799557b3e7328de50f6e5f90c1098ed485fcc9
Zé William <josewilliam@justto.com.br>
2021-03-31 12:31:56 -0300

    SAAS-3552 🚧 Adiciona dialog de edição dos dados pessoais.

---
+++ src/components/dialogs/JusEditUserDialog.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-dialog
+    :title="$t('dialog.title.my-data')"
+    :visible.sync="dialogVisible"
+    custom-class="edit-user-modal"
+    :close-on-click-modal="false"
+    append-to-body
+    destroy-on-close
+  >
+    <JusEdituser />
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  components: {
+    JusEdituser: () => import('@/components/layouts/JusEditUser')
+  },
+  data: () => ({
+    dialogVisible: false
+  }),
+  methods: {
+    open() {
+      this.dialogVisible = true
+    }
+  }
+}
+</script>
