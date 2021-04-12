d948ba18cf1808531aec71a63d384b57c076a230
Zé William <josewilliam@justto.com.br>
2021-04-12 17:59:34 -0300

    ♻️ Adiciona banco UNICRED à lista de bancos do sistema.

--- src/constants/banksList.js
+++ src/constants/banksList.js
@@ -253,5 +253,6 @@ export default
   { code: '260', name: 'NUBANK' },
   { code: '102', name: 'XP INVESTIMENTOS' },
   { code: '336', name: 'BANCO C6 S.A' },
-  { code: '085', name: 'COOP CENTRAL AILOS' }
+  { code: '085', name: 'COOP CENTRAL AILOS' },
+  { code: '136', name: 'UNICRED COOPERATIVA LTDA' }
 ]
