0244b78f3e87ea92bf52a93deed69e4129ae9147
Lucas Israel <lucas@justto.com.br>
2021-04-15 15:37:24 -0300

    SAAS-3760 :bug: Corrige atualização de valor da proposta em disputas com alçadas zeradas

--- src/models/negotiations/overview/TicketOverviewLastOfferVm.js
+++ src/models/negotiations/overview/TicketOverviewLastOfferVm.js
@@ -15,17 +15,31 @@ export default class LastOfferVm {
     plaintiffOffer,
     lastCounterOfferRoleId,
     lastCounterOfferValue,
-    lastCounterOfferName
+    lastCounterOfferName,
+    disputeUpperRange
   }) {
-    this.defendantOffer = new ProposalVm(defendantOffer || {
-      roleId: lastOfferRoleId,
-      value: lastOfferValue,
-      name: lastOfferName
-    })
-    this.plaintiffOffer = new ProposalVm(plaintiffOffer || {
-      roleId: lastCounterOfferRoleId,
-      value: lastCounterOfferValue,
-      name: lastCounterOfferName
-    })
+    if (disputeUpperRange) {
+      this.defendantOffer = new ProposalVm(defendantOffer || {
+        roleId: lastOfferRoleId,
+        value: lastOfferValue,
+        name: lastOfferName
+      })
+      this.plaintiffOffer = new ProposalVm(plaintiffOffer || {
+        roleId: lastCounterOfferRoleId,
+        value: lastCounterOfferValue,
+        name: lastCounterOfferName
+      })
+    } else {
+      this.plaintiffOffer = new ProposalVm(defendantOffer || {
+        roleId: lastOfferRoleId,
+        value: lastOfferValue,
+        name: lastOfferName
+      })
+      this.defendantOffer = new ProposalVm(plaintiffOffer || {
+        roleId: lastCounterOfferRoleId,
+        value: lastCounterOfferValue,
+        name: lastCounterOfferName
+      })
+    }
   }
 }
