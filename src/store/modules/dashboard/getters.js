e05aa08c9278f87486d93eb30de4e3520e5351a7
Zé William <josewilliam@justto.com.br>
2021-04-27 11:16:33 -0300

    SAAS-3694 🚧 Mockup dos dados do NPS.

--- src/store/modules/dashboard/getters.js
+++ src/store/modules/dashboard/getters.js
@@ -5,6 +5,24 @@ const dashboardGetters = {
     const datasets = state.chartsDatasets.find(cd => cd.name === chartName)
     if (datasets) return JSON.parse(JSON.stringify(datasets))
     return {}
+  },
+  getNpsSummary: () => {
+    return {
+      name: 'WORKSPACE_SUMMARIES',
+      data: {
+        labels: [],
+        datasets: [
+          { label: 'NPS_PROMOTER_PERCENTAGE', data: [20], width: 25 },
+          { label: 'NPS_DETRACTOR_PERCENTAGE', data: [60], width: 25 },
+          { label: 'NPS_PASSIVE_PERCENTAGE', data: [20], width: 25 },
+          { label: 'UPPER_RANGE_AVG', data: [20045.322962962964], width: 50 },
+          { label: 'SETTLED_DEALS_AVG', data: [16451.76160493827], width: 50 },
+          { label: 'SAVINGS_TOTAL', data: [291078.47], width: 100 },
+          { label: 'SAVINGS_PERCENTAGE', data: [22], width: 100 }
+        ]
+      },
+      options: {}
+    }
   }
 }
 
