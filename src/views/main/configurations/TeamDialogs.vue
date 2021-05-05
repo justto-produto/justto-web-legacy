fb15d253f9b818864a52433e00921d84e1d1b2cf
Ladgelson <micaiasladgelsondasilva@gmail.com>
2021-05-05 10:08:49 -0300

    SAAS-3515💄Adiciona termos (privacidade e uso) no convite para workspace

--- src/views/main/configurations/TeamDialogs.vue
+++ src/views/main/configurations/TeamDialogs.vue
@@ -37,12 +37,14 @@
           </el-select>
         </el-form-item>
         <el-form-item>
-          <el-checkbox
-            v-model="acceptTerms"
-          />
-          <span>
-            Eu, {{ personName }}, estou ciente que ao cadastrar uma nova pessoa no time, esta pessoa irá ter acesso as disputas da minha conta e estou de acordo com os <a href="https://justto.com.br/termos-de-uso">termos de uso</a> da plataforma e com a <a href="https://justto.com.br/poilitica-privacidade">política de privacidade</a>.
-          </span>
+          <div class="acceptTerms">
+            <el-checkbox
+              v-model="acceptTerms"
+            />
+            <span>
+              Eu, {{ personName }}, estou ciente que ao cadastrar uma nova pessoa no time, esta pessoa irá ter acesso as disputas da minha conta e estou de acordo com os <a href="https://justto.com.br/termos-de-uso">termos de uso</a> da plataforma e com a <a href="https://justto.com.br/poilitica-privacidade">política de privacidade</a>.
+            </span>
+          </div>
         </el-form-item>
       </el-form>
       <span slot="footer">
@@ -157,8 +159,8 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-  .el-form-item__content {
-    line-height: 25px;
-  }
+<style scoped>
+.acceptTerms {
+  line-height: 25px;
+}
 </style>
