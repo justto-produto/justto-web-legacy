b9c22a35960daf1c483bc269d0523cf95630d52c
Zé William <josewilliam@justto.com.br>
2021-05-27 11:57:56 -0300

    SAAS-3939  ✨ Finaliza implementação dos plugins de menção/preview.

--- src/utils/mixins/ckeditor.js
+++ src/utils/mixins/ckeditor.js
@@ -50,10 +50,10 @@ export default {
           TextTransformation,
           Underline,
           Mention,
-          this.MentionCustomization,
-          this.PreviewPlugin
+          this.useMenstionPlugin ? this.MentionCustomization : () => {},
+          this.usePreviewPlugin ? this.PreviewPlugin : () => {}
         ],
-        mention: {
+        mention: this.useMenstionPlugin ? {
           feeds: [
             {
               marker: '{',
@@ -62,7 +62,7 @@ export default {
               itemRenderer: this.customItemRenderer
             }
           ]
-        },
+        } : {},
         toolbar: {
           items: [
             'bold',
@@ -172,13 +172,11 @@ export default {
 
     PreviewPlugin(editor) {
       editor.ui.componentFactory.add('preview', locale => {
-        // const imageIcon = () => import('@/assets/icons/ic-eye-white.svg')
         const view = new ButtonView(locale)
 
         view.set({
           label: 'Visualizar Mensagem',
-          // TODO: Ajustar o ícone.
-          icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13.2 9.5"><path d="M12 7h-.5V.8c0-.1-.1-.1-.2-.1H1.8c-.1 0-.1.1-.1.1V7h-.6V.8c0-.3.3-.6.6-.6h9.5c.3 0 .7.3.7.6V7zM12.4 9.5H.7c-.3 0-.7-.4-.7-.7v-1h6.1v.4H7v-.4h6v1c0 .3-.3.7-.6.7zM.5 8.3v.5c0 .1.1.2.2.2h11.7c.1 0 .1-.1.1-.2v-.5H7.4c0 .2-.2.4-.4.4h-.9c-.2 0-.4-.2-.4-.4H.5z"/><path d="M5.4 4.1c0 .2-.2.4-.4.4s-.4-.2-.4-.4.2-.4.4-.4.4.2.4.4" fill-rule="evenodd" clip-rule="evenodd"/><g><path d="M5.8 6c-.1-.1-.3-.2-.4-.4l.5-.3c0 .1.1.2.2.2.1.1.2.1.4.1s.4-.1.5-.2c.1-.2.2-.4.2-.6V1.9h.6v2.9c0 .3-.1.5-.2.7-.1.2-.2.4-.4.5-.2.1-.5.2-.7.2-.3 0-.5-.1-.7-.2z"/></g></svg>',
+          icon: '<svg xmlns="http://www.w3.org/2000/svg" width="18.409" height="18"><path data-name="Retângulo 67" fill="none" d="M.204 0h18v18h-18z"/><g data-name="Grupo 3101"><g data-name="Grupo 26"><path data-name="Caminho 253" d="M9.205 12.018a3.043 3.043 0 10-3.019-3.043 3.031 3.031 0 003.019 3.043z" fill="none" stroke="#000" stroke-width=".8"/><path data-name="Caminho 254" d="M9.204 10.745a1.839 1.839 0 10-1.781-1.838 1.814 1.814 0 001.781 1.838z" fill="#191919" fill-rule="evenodd"/><path data-name="Caminho 11629" d="M9.761 9.397a1.032 1.032 0 10-1-1.032 1.019 1.019 0 001 1.032z" fill="#fff" fill-rule="evenodd"/><path data-name="Caminho 255" d="M17.909 8.902a10.174 10.174 0 00-17.409 0 10.174 10.174 0 0017.409 0z" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round"/></g></g></svg>',
           tooltip: true
         })
 
