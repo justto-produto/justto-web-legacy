243738f327a278d11a014eb2d6a49c0638baed37
Zé William <josewilliam@justto.com.br>
2021-05-27 11:56:14 -0300

    SAAS-3939 🚧 Adiciona componente de preview de mensagens.

---
+++ src/components/dialogs/JusMessagePreviewDialog.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-dialog
+    :visible.sync="isVisible"
+    title="Preview"
+    width="50%"
+    destroy-on-close
+  >
+    <div
+      class="preview"
+      v-html="previewMessage"
+    />
+  </el-dialog>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters({
+      previewMessage: 'getPreviewMessage',
+      showPreview: 'canShowPreview'
+    }),
+
+    isVisible: {
+      get() {
+        return this.showPreview
+      },
+      set(visibility) {
+        this.setShowPreview(visibility)
+      }
+    }
+  },
+  methods: {
+    ...mapActions(['setShowPreview']),
+  }
+}
+</script>
