8e8ef4c9b2ae09b7ae9a2540710d571ff990bd6b
Zé William <josewilliam@justto.com.br>
2021-05-31 12:46:01 -0300

    SAAS-4000 🤡 Adiciona dado de cancelados mockado.

--- src/store/modules/billing/getters.js
+++ src/store/modules/billing/getters.js
@@ -6,7 +6,7 @@ const billingGetters = {
   getCurrentCustomer: state => new ClientModel(state.currentCustomer),
   getPlans: state => state.plans,
   transactions: state => state.currentCustomer.transactions,
-  billingDashboard: state => state.billingDashboard,
+  billingDashboard: state => [...state.billingDashboard, { archived: null, revenue: null, title: 'Cancelados', total: 0, type: 'CANCELED' }],
   tableLoading: state => state.tableLoading,
   currentWorkspace: state => state.query.workspaceId
 }
