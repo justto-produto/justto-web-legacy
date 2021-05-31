b6c3adca52bc5dbd7dab1d4e472115e2e4ac2b89
Zé William <josewilliam@justto.com.br>
2021-05-31 14:29:59 -0300

    SAAS-4000 🤡 Ajusta dados mockados.

--- src/store/modules/billing/getters.js
+++ src/store/modules/billing/getters.js
@@ -6,7 +6,7 @@ const billingGetters = {
   getCurrentCustomer: state => new ClientModel(state.currentCustomer),
   getPlans: state => state.plans,
   transactions: state => state.currentCustomer.transactions,
-  billingDashboard: state => [...state.billingDashboard, { archived: null, revenue: null, title: 'Cancelados', total: 0, type: 'CANCELED' }],
+  billingDashboard: state => [...state.billingDashboard, { archived: null, revenue: null, title: 'Indicativo de baixa', total: 0, type: 'LAWSUIT_DROPPED' }],
   tableLoading: state => state.tableLoading,
   currentWorkspace: state => state.query.workspaceId
 }
