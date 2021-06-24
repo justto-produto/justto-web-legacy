36853cc06a318cc88aee46acbf2f1ad27af571fc
Ladgelson <micaiasladgelsondasilva@gmail.com>
2021-06-24 16:32:02 -0300

    SAAS-4073 ✨ Cria component de dialog + slider de novidades

---
+++ src/components/dialogs/SlideInfo.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-dialog
+    :close-on-click-modal="false"
+    :visible.sync="isVisible"
+    append-to-body
+    custom-class="namesake-dialog"
+    title="Tratar homônimo"
+    width="70%"
+  >
+    <h1>Customizando suas mensagens rápidas</h1>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import '@/styles/colors.scss';
+
+</style>
