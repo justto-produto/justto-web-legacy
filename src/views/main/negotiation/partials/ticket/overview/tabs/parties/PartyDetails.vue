036e58052b5468d742402a381dff654091281690
Zé William <josewilliam@justto.com.br>
2021-07-12 14:50:02 -0300

    SAAS-4243 🐛 Adiciona validação para OABs na tela nova.

--- src/views/main/negotiation/partials/ticket/overview/tabs/parties/PartyDetails.vue
+++ src/views/main/negotiation/partials/ticket/overview/tabs/parties/PartyDetails.vue
@@ -665,8 +665,12 @@ export default {
     oabMask(value = '') {
       const oab = value?.replace(/[^\w*]/g, '').toUpperCase()
 
+      console.log(oab, oab.charAt(4))
+
       if (/[ABDENPabdenp]/.test(oab.charAt(2))) {
         return '##D.###/AA'
+      } else if (/[A-Z]/.test(oab.charAt(4))) {
+        return '#.###/AA'
       } else if (/[A-Z]/.test(oab.charAt(5))) {
         return '##.###/AA'
       } else {
