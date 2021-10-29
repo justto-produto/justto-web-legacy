513a3d2cc2e931b1833f15ec97e164ed10c5bf1b
Will Vitorino <josewilliam@justto.com.br>
2021-10-29 12:09:30 -0300

    SAAS-4466  🚧 Tooltip de contato bloqueado no Ticket.

--- src/views/main/negotiation/partials/ticket/overview/tabs/parties/PartyContacts.vue
+++ src/views/main/negotiation/partials/ticket/overview/tabs/parties/PartyContacts.vue
@@ -55,7 +55,37 @@
             />
           </el-popover>
         </el-tooltip>
+
+        {{ contact.blocked }}
+
+        <el-popover
+          v-if="contact.blocked"
+          placement="top-start"
+          trigger="hover"
+          content="Este contato foi bloqueado pelo usuário."
+        >
+          <TextInlineEditor
+            slot="reference"
+            v-model="contact[model]"
+            :mask="mask"
+            :filter="filter"
+            :is-editable="!disabled"
+            :is-deletable="!disabled"
+            :class="{
+              'party-contacts__infoline-data--selected': mappedRecipients.includes(contact[model]),
+              'party-contacts__infoline-data--disabled': !contact.isValid || contact.blocked,
+              'party-contacts__infoline-data--secondary': !contact.isMain || contact.blocked
+            }"
+            class="party-contacts__infoline-data"
+            @call="makeCall"
+            @change="updateContact(contact.id, $event)"
+            @delete="removeContact(contact.id)"
+            @click="selectContact(contact[model], contact.isValid, (contact.isMain && !contact.blocked))"
+          />
+        </el-popover>
+
         <TextInlineEditor
+          v-else
           v-model="contact[model]"
           :mask="mask"
           :filter="filter"
@@ -63,14 +93,14 @@
           :is-deletable="!disabled"
           :class="{
             'party-contacts__infoline-data--selected': mappedRecipients.includes(contact[model]),
-            'party-contacts__infoline-data--disabled': !contact.isValid,
-            'party-contacts__infoline-data--secondary': !contact.isMain
+            'party-contacts__infoline-data--disabled': !contact.isValid || contact.blocked,
+            'party-contacts__infoline-data--secondary': !contact.isMain || contact.blocked
           }"
           class="party-contacts__infoline-data"
           @call="makeCall"
           @change="updateContact(contact.id, $event)"
           @delete="removeContact(contact.id)"
-          @click="selectContact(contact[model], contact.isValid, contact.isMain)"
+          @click="selectContact(contact[model], contact.isValid, (contact.isMain && !contact.blocked))"
         />
       </div>
     </span>
