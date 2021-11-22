3bb603b17089020bd4e7191cf3212efb5b6006d7
Will Vitorino <josewilliam@justto.com.br>
2021-11-22 15:46:10 -0300

    SAAS-2983 ♻️ Componentiza aba de informações da disputa.

--- src/views/main/dispute/partials/DisputeOverview/DisputeOverview.vue
+++ src/views/main/dispute/partials/DisputeOverview/DisputeOverview.vue
@@ -16,6 +16,7 @@
           @click="openRemoveDisputeDialog()"
         />
       </el-tooltip>
+
       <span class="dispute-overview-view__subtitle">
         <dispute-code-link
           v-if="dispute && !!dispute.code"
@@ -26,6 +27,7 @@
         />
       </span>
     </h2>
+
     <el-dialog
       :close-on-click-modal="false"
       :show-close="false"
@@ -72,6 +74,7 @@
         </el-button>
       </span>
     </el-dialog>
+
     <div
       v-loading="loading || linkBankAccountLoading"
       class="dispute-overview-view__loading"
@@ -91,316 +94,13 @@
               <i class="el-icon-info" />
             </el-tooltip>
           </span>
-          <div>
-            <div
-              class="dispute-overview-view__info-line"
-              data-testid="dispute-infoline"
-            >
-              <span class="title">Etiquetas:</span>
-              <jus-tags />
-            </div>
-            <div
-              v-if="dispute.createAt"
-              class="dispute-overview-view__info-line"
-              data-testid="dispute-infoline"
-            >
-              <span class="title">Data de importação:</span>
-              <span>{{ dispute.createAt.dateTime | moment('DD/MM/YY') }}</span>
-            </div>
-            <div
-              class="dispute-overview-view__info-line"
-              data-testid="dispute-infoline"
-            >
-              <span class="title">Processo:</span>
-              <dispute-code-link
-                :code="dispute.code"
-                :custom-style="{ fontSize: '14px', marginLeft: '12px'}"
-                @openTimeline="openTimelineModal"
-              />
-            </div>
-            <div
-              v-if="campaign"
-              class="dispute-overview-view__info-line"
-              data-testid="dispute-infoline"
-            >
-              <span class="title">Campanha:</span>
-              <span>{{ campaign.name }}</span>
-            </div>
-            <div
-              v-if="dispute.strategyName"
-              class="dispute-overview-view__info-line"
-              data-testid="dispute-infoline"
-            >
-              <span class="title">Estratégia:</span>
-              <span data-testid="overview-strategy">{{ dispute.strategyName }}</span>
-            </div>
-            <div
-              class="dispute-overview-view__info-line"
-              data-testid="dispute-infoline"
-            >
-              <span class="title">Status:</span>
-              <el-tooltip
-                :disabled="!statusTooltip()"
-                popper-class="info-line__status-tooltip"
-              >
-                <span
-                  slot="content"
-                  v-html="statusTooltip()"
-                />
-                <span data-testid="overview-status">
-                  <span>{{ $t('occurrence.type.' + dispute.status) | capitalize }}</span>
-                  <span v-if="dispute.paused">(pausada)</span>
-                  <span
-                    v-if="statusTooltip()"
-                    class="el-icon-question"
-                  />
-                </span>
-              </el-tooltip>
-            </div>
-            <div
-              class="dispute-overview-view__info-line"
-              data-testid="dispute-infoline"
-            >
-              <span class="title">{{ $tc('UPPER_RANGE', isRecoveryStrategy) }}:</span>
-              <span data-testid="overview-upperrange">{{ dispute.disputeUpperRange | currency }}</span>
-            </div>
-            <div
-              class="dispute-overview-view__info-line"
-              data-testid="dispute-infoline"
-            >
-              <span class="title">Valor proposto<span v-if="dispute.lastOfferName"> por {{ dispute.lastOfferName | firstName }}</span>:</span>
-              <span data-testid="overview-proposal">
-                <el-tooltip
-                  v-if="dispute.lastOfferName"
-                  :content="'Proposto por: ' + dispute.lastOfferName"
-                >
-                  <jus-avatar-user
-                    :name="dispute.lastOfferName"
-                    size="mini"
-                  />
-                </el-tooltip>
-                {{ dispute.lastOfferValue | currency }}
-              </span>
-            </div>
-            <div
-              v-if="dispute.lastCounterOfferValue > 0 && dispute.disputeUpperRange"
-              class="dispute-overview-view__info-line"
-              data-testid="dispute-infoline"
-            >
-              <span class="title">Contraproposta<span v-if="dispute.lastCounterOfferName"> por {{ dispute.lastCounterOfferName | firstName }}</span>:</span>
-              <span data-testid="overview-counterproposal">
-                <el-tooltip
-                  v-if="dispute.lastCounterOfferName"
-                  :content="'Proposto por: ' + dispute.lastCounterOfferName"
-                >
-                  <jus-avatar-user
-                    :name="dispute.lastCounterOfferName"
-                    size="mini"
-                  />
-                </el-tooltip>
-                {{ dispute.lastCounterOfferValue | currency }}
-              </span>
-            </div>
-            <div
-              v-if="(dispute.status === 'ACCEPTED' || dispute.status === 'CHECKOUT' || dispute.status === 'SETTLED') && dispute.disputeDealValue"
-              class="dispute-overview-view__info-line"
-            >
-              <span class="title">Valor do acordo:</span>
-              <span>{{ dispute.disputeDealValue | currency }}</span>
-            </div>
-            <div
-              class="dispute-overview-view__info-line"
-              data-testid="dispute-infoline"
-            >
-              <span class="title">Fim da negociação:</span>
-              <span
-                v-if="dispute.expirationDate"
-                data-testid="overview-expirationdate"
-              >{{ dispute.expirationDate.dateTime | moment('DD/MM/YY') }}</span>
-            </div>
-            <div
-              v-if="dispute.materialDamage"
-              class="dispute-overview-view__info-line"
-              data-testid="dispute-infoline"
-            >
-              <span class="title">Dano material:</span>
-              <span
-                v-if="dispute.materialDamage"
-                data-testid="overview-materialdamage"
-              >{{ dispute.materialDamage | currency }}</span>
-            </div>
-            <div
-              v-if="dispute.moralDamage"
-              class="dispute-overview-view__info-line"
-              data-testid="dispute-infoline"
-            >
-              <span class="title">Dano moral:</span>
-              <span
-                v-if="dispute.moralDamage"
-                data-testid="overview-moralDamage"
-              >{{ dispute.moralDamage | currency }}</span>
-            </div>
-            <div
-              v-if="dispute.provisionedValue > 0"
-              class="dispute-overview-view__info-line"
-              data-testid="dispute-infoline"
-            >
-              <span class="title">Valor provisionado:</span>
-              <span data-testid="overview-provisioned-value">{{ dispute.provisionedValue | currency }}</span>
-            </div>
-            <div
-              v-if="dispute.requestedValue"
-              class="dispute-overview-view__info-line"
-              data-testid="dispute-infoline"
-            >
-              <span class="title">Valor do processo:</span>
-              <span
-                v-if="dispute.requestedValue"
-                data-testid="overview-requestedvalue"
-              >{{ dispute.requestedValue | currency }}</span>
-            </div>
-            <div
-              v-if="dispute.externalId"
-              class="dispute-overview-view__info-line"
-              data-testid="dispute-infoline"
-            >
-              <span class="title">Código interno:</span>
-              <span
-                v-if="dispute.externalId"
-                data-testid="overview-externalid"
-              >{{ dispute.externalId }}</span>
-            </div>
-            <div
-              class="dispute-overview-view__info-line"
-              data-testid="dispute-infoline"
-            >
-              <span class="title">Configurações:</span>
-              <span
-                v-if="dispute"
-                class="configurations"
-              >
-                Enriquecer automaticamente na importação?
-                <div><i :class="skipEnrichment ? 'el-icon-close' : 'el-icon-check'" /> {{ skipEnrichment ? 'Não' : 'Sim' }}</div>
-                Somente depósito em conta-corrente?
-                <div><i :class="dispute && dispute.campaign && dispute.campaign.denySavingDeposit ? 'el-icon-check' : 'el-icon-close'" /> {{ dispute && dispute.campaign && dispute.campaign.denySavingDeposit ? 'Sim' : 'Não ' }}</div>
-                Mensagens somente em horário comercial?
-                <div><i :class="dispute && dispute.campaign && dispute.campaign.businessHoursEngagement ? 'el-icon-check' : 'el-icon-close'" /> {{ dispute && dispute.campaign && dispute.campaign.businessHoursEngagement ? 'Sim' : 'Não' }}</div>
-                Contatar autor?
-                <div>
-                  <i :class="(dispute.contactPartyWhenNoLowyer || dispute.contactPartyWhenInvalidLowyer) ? 'el-icon-check' : 'el-icon-close'" />
-                  <span v-if="dispute.alwaysContactParty">
-                    Sempre
-                  </span>
-                  <span v-else-if="dispute.contactPartyWhenNoLowyer && dispute.contactPartyWhenInvalidLowyer">
-                    Quando não houver advogado ou não for possível contatar o advogado existente
-                  </span>
-                  <span v-else-if="dispute.contactPartyWhenNoLowyer && !dispute.contactPartyWhenInvalidLowyer">
-                    Somente quando não houver advogado constituído
-                  </span>
-                  <span v-else-if="!dispute.contactPartyWhenNoLowyer && dispute.contactPartyWhenInvalidLowyer">
-                    Somente quando não for possível contatar o advogado existente
-                  </span>
-                  <span v-else>
-                    Nunca
-                  </span>
-                </div>
-              </span>
-            </div>
-            <div
-              v-if="computedDescription"
-              class="dispute-overview-view__info-line"
-            >
-              <span class="title">Descrição:</span>
-              <span>
-                <span
-                  :class="{ 'right': computedDescription.length < 25 }"
-                  data-testid="overview-description"
-                >
-                  {{ computedDescription }}
-                  <span v-if="dispute.description.length > 140">
-                    <a
-                      href="#"
-                      class="dispute-overview-view__see-more"
-                      @click.prevent="descriptionCollapse = !descriptionCollapse"
-                    >
-                      {{ descriptionCollapse ? 'ver mais': 'ver menos' }}
-                      <i :class="descriptionCollapse ? 'el-icon-arrow-down': 'el-icon-arrow-up'" />
-                    </a>
-                  </span>
-                </span>
-              </span>
-            </div>
-            <div
-              v-if="dispute.classification"
-              class="dispute-overview-view__info-line"
-            >
-              <span class="title">Classificação:</span>
-              <span class="classification">
-                {{ dispute.classification.name }}
-                <div
-                  v-for="subClassification in dispute.classification.classificationDetails"
-                  :key="subClassification.id"
-                >
-                  <i class="el-icon-right" />
-                  {{ subClassification.name }}
-                </div>
-              </span>
-            </div>
-            <div
-              v-if="dispute.bankAccounts && dispute.bankAccounts.length"
-              class="dispute-overview-view__info-line"
-            >
-              <span class="title">Conta(s) bancária(s):</span>
-              <el-collapse value="0">
-                <el-collapse-item
-                  v-for="(bankAccount, index) in dispute.bankAccounts"
-                  :key="`${index}-${bankAccount.id}`"
-                  :name="index"
-                  class="dispute-overview-view__bank-collapse"
-                >
-                  <template slot="title">
-                    <div>
-                      {{ bankAccount.name || bankAccount.document | cpfCnpj }}
-                      <span>
-                        {{ bankAccount.bank }} <span v-if="bankAccount.agency">|</span>
-                        {{ bankAccount.agency }} <span v-if="bankAccount.number">|</span>
-                        {{ bankAccount.number }}
-                      </span>
-                    </div>
-                  </template>
-                  <span class="bank-info">
-                    <span v-show="bankAccount.name">
-                      <strong>Nome:</strong> {{ bankAccount.name }} <br>
-                    </span>
-                    <span v-show="bankAccount.email">
-                      <strong>E-mail:</strong> {{ bankAccount.email }} <br>
-                    </span>
-                    <strong>Documento:</strong> {{ bankAccount.document | cpfCnpj }} <br>
-                    <strong>Banco:</strong> {{ bankAccount.bank }} <br>
-                    <strong>Agência:</strong> {{ bankAccount.agency }} <br>
-                    <strong>Conta:</strong> {{ bankAccount.number }} <br>
-                    <strong>Tipo:</strong> {{ bankAccount.type === 'SAVING' ? 'Poupança' : 'Corrente' }} <br>
-                  </span>
-                </el-collapse-item>
-              </el-collapse>
-            </div>
-          </div>
-          <el-tooltip
-            :disabled="dispute.status !== 'PRE_NEGOTIATION'"
-            content="Disputas em pré negociação não podem ser editadas"
-          >
-            <div class="dispute-overview-view__actions">
-              <el-button
-                :disabled="dispute.status === 'PRE_NEGOTIATION'"
-                type="primary"
-                data-testid="edit-dispute"
-                @click="openDisputeDialog()"
-              >
-                Editar
-              </el-button>
-            </div>
-          </el-tooltip>
+
+          <general-info-tab
+            @openDispute="openDisputeDialog"
+            @openTimeline="openTimelineModal"
+          />
         </el-tab-pane>
+
         <!-- PARTES DA DISPUTA -->
         <el-tab-pane
           name="roles"
@@ -1760,12 +1460,14 @@
         :document-numbers="documentNumbers"
         :oabs="oabs"
       />
+
       <jus-timeline
         v-if="disputeTimelineModal"
         v-model="disputeTimelineModal"
         :code="dispute.code"
         @update:contact="restartEngagementFromTimeline"
       />
+
       <associate-contacts-modal
         v-model="showAssociateContacts"
         :current="dispute && dispute.properties && dispute.properties['CONTATOS ASSOCIADOS'] ? dispute.properties['CONTATOS ASSOCIADOS'] : false"
@@ -1793,11 +1495,11 @@ export default {
     DisputeAddRole: () => import('../DisputeAddRole'),
     DisputeCodeLink: () => import('@/components/buttons/DisputeCodeLink'),
     DisputeProperties: () => import('../DisputeProperties'),
-    JusTags: () => import('@/components/others/JusTags'),
     JusTimeline: () => import('@/components/JusTimeline/JusTimeline'),
     JusVexatiousAlert: () => import('@/components/dialogs/JusVexatiousAlert'),
     LawyerDetail: () => import('@/components/others/LawyerDetail'),
-    AssociateContactsModal: () => import('@/components/dialogs/AssociateContactsModal')
+    AssociateContactsModal: () => import('@/components/dialogs/AssociateContactsModal'),
+    generalInfoTab: () => import('./sections/generalInfoTab.vue')
   },
 
   mixins: [restartEngagement],
@@ -1947,15 +1649,10 @@ export default {
       ]
     },
 
-    campaign() {
-      return this.dispute?.campaign
-    },
-    skipEnrichment() {
-      return this.dispute?.campaign?.skipEnrichment
-    },
     onlineList() {
       return Object.keys(this.onlineDocuments) || []
     },
+
     contactsMetadataCount() {
       const { phones, emails } = this.disputeMetadata
       return phones.length + emails.length
@@ -2018,14 +1715,7 @@ export default {
     isValidStrategie() {
       return (this.strategies || []).map(s => s.id).includes(this.dispute.strategyId)
     },
-    computedDescription() {
-      if (this.dispute.description && this.dispute.description.length > 140) {
-        if (this.descriptionCollapse) {
-          return this.dispute.description.substring(0, 140) + '...'
-        }
-      }
-      return this.dispute.description
-    },
+
     disputeRolesSort() {
       if (this.dispute.disputeRoles) {
         let sortedArray = this.dispute.disputeRoles.slice(0) || []
