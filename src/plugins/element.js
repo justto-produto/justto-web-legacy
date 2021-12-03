eaf698821dcc39030a598ecaf8d4d3954ba6ecb2
Will Vitorino <josewilliam@justto.com.br>
2021-12-03 13:36:44 -0300

    SAAS-4696 📦 Atualiza Element.

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -3,6 +3,6 @@ import Element from 'element-ui'
 import i18n from '@/plugins/vueI18n'
 
 import '@/styles/variables.scss'
-import 'element-theme-chalk/lib/display.css'
+import 'element-ui/lib/theme-chalk/display.css'
 
 Vue.use(Element, { i18n: (key, value) => i18n.t(key, value) })
