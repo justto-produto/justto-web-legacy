4e190a920103583352e1e28fb889bb5a9f8e8d28
Will Vitorino <josewilliam@justto.com.br>
2022-05-31 10:53:04 -0300

    SAAS-5089 ♻️ Refatora estilo da Obtigação de fazer.

--- src/views/main/negotiation/partials/ticket/overview/Overview.vue
+++ src/views/main/negotiation/partials/ticket/overview/Overview.vue
@@ -65,7 +65,7 @@
     />
 
     <OverviewObf
-      v-if="ticket.strategy && ticket.strategy.isObrigacaoFazer"
+      v-if="ticket.strategy && !ticket.strategy.isObrigacaoFazer"
       v-model="ticket.description"
     />
 
