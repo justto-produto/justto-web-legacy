9fa82590a32bea356af6a4edc85e47884ed4179e
Will Vitorino <josewilliam@justto.com.br>
2022-08-04 12:07:40 -0300

    SAAS-2238 ♻️ Vai para a Home customizada ao mudar de Workspace.

--- src/components/dialogs/JusChangeWorkspace.vue
+++ src/components/dialogs/JusChangeWorkspace.vue
@@ -53,14 +53,18 @@ export default {
       default: () => (false)
     }
   },
+
   data: () => ({
     selectedWorkspace: ''
   }),
+
   computed: {
     ...mapGetters({
       loggedWorkspaceId: 'workspaceId',
-      workspaces: 'getUserWorkspaces'
+      workspaces: 'getUserWorkspaces',
+      customHome: 'getCustomHome'
     }),
+
     isVisible: {
       get() {
         return this.visible
@@ -70,25 +74,32 @@ export default {
       }
     }
   },
+
   methods: {
     ...mapActions({
       getMyWorkspaces: 'myWorkspace'
     }),
+
     goToWorkspace(workspace) {
       const loading = this.$loading({
         lock: true,
         text: 'Alterando Equipe...'
       })
+
       const oldWorkspace = this.$store.getters.workspaceTeamName
+
       if (workspace.workspace) {
         this.$store.commit('setWorkspace', workspace.workspace)
       }
+
       if (workspace.profile) {
         this.$store.commit('setProfile', workspace.profile)
       }
+
       if (workspace.person) {
         this.$store.commit('setLoggedPerson', workspace.person)
       }
+
       this.$store.dispatch('getWorkspaceMembers')
         .then(() => {
           this.$jusSegment('Troca de time/workspace', {
@@ -97,39 +108,40 @@ export default {
         }).catch(error => {
           this.$jusNotification({ error })
         }).finally(() => {
-          if (this.$route?.params?.id) {
-            const { href } = window.location
-            const newPath = href.split('#')[0] + '#/negotiation'
-            window.location.replace(newPath)
-          }
+          const { href } = window.location
+
           this.isVisible = false
+
           setTimeout(() => {
             loading.close()
           }, 1000)
+
+          window.location.replace(href.split('#')[0] + `#${this.customHome || '/negotiation'}`)
           window.location.reload()
         })
     },
+
     getMembersAndRedirect() {
       const loading = this.$loading({
         lock: true,
         text: 'Alterando Equipe...'
       })
+
       const selectedWorkspace = this.workspaces[this.selectedWorkspace]
+
       if (selectedWorkspace.person) {
         this.goToWorkspace(selectedWorkspace)
       } else {
         this.$store.dispatch('ensureWorkspaceAccesss', selectedWorkspace.workspace.id).then((res) => {
           this.$store.commit('setToken', res)
+
           this.getMyWorkspaces().then(response => {
             const updatedWorkspace = response.find(el => {
               return el.workspace.id === selectedWorkspace.workspace.id
             })
+
             this.goToWorkspace(updatedWorkspace)
-          }).finally(() => {
-            setTimeout(() => {
-              loading.close()
-            }, 1000)
-          })
+          }).finally(() => { setTimeout(loading.close, 1000) })
         })
       }
     }
