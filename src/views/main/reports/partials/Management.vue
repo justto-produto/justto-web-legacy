92f30aa5450619517be70a9642e661b86cbea707
Will Vitorino <josewilliam@justto.com.br>
2022-08-29 18:28:42 -0300

    SAAS-5283 ✨ Adiciona página dos relatórios.

---
+++ src/views/main/reports/partials/Management.vue
@@ -0,0 +1,15 @@
+<template>
+  <iframe
+    src="https://datastudio.google.com/embed/reporting/bd7283fc-5c62-4ce0-b834-b6af01713c39/page/p_s4mc6x4moc"
+    frameborder="0"
+    allowfullscreen
+  />
+</template>
+
+<style lang="scss" scoped>
+iframe {
+  border: none;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/views/main/reports/partials/Managerial.vue
@@ -0,0 +1,15 @@
+<template>
+  <iframe
+    src="https://datastudio.google.com/embed/reporting/dfc525b5-6407-43f7-93e5-13de85185987/page/p_tupwqk8loc"
+    frameborder="0"
+    allowfullscreen
+  />
+</template>
+
+<style lang="scss" scoped>
+iframe {
+  border: none;
+  width: 100%;
+  height: 100%;
+}
+</style>
