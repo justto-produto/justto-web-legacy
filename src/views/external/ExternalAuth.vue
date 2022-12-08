1fa8ab63fca2c8c5d56a3a6e44e313db264b61ae
Will Vitorino <josewilliam@justto.com.br>
2022-12-08 12:20:26 -0300

    SAAS-5370 🚧 Lida com informações de login.

--- src/views/external/ExternalAuth.vue
+++ src/views/external/ExternalAuth.vue
@@ -1,19 +1,72 @@
 <template>
-  <article class="external-auth__container">
-    <amplify-authenticator>
-      <div>
-        My App here.
-        <amplify-sign-out />
-      </div>
-    </amplify-authenticator>
-  </article>
+  <div class="external-view external-auth__container">
+    <el-container>
+      <el-aside
+        width="50%"
+        class="hidden-sm-and-down"
+      >
+        <jus-sidenav-external />
+      </el-aside>
+
+      <el-main class="display-flex">
+        <amplify-authenticator class="authenticator__container">
+          <div>
+            Tela de selecionar Workspace.
+            <amplify-sign-out />
+          </div>
+        </amplify-authenticator>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <script>
+import { onAuthUIStateChange } from '@aws-amplify/ui-components'
+
 export default {
+  components: {
+    JusSidenavExternal: () => import('@/components/layouts/JusSidenavExternal')
+  },
+
+  created() {
+    onAuthUIStateChange((authState, authData) => {
+      console.log({ authState, authData })
+      switch (authState) {
+        case 'signedin':
+          this.handleSignedIn(authData)
+          break
+        case 'signedout':
+          this.handleSignedOut()
+          break
+        default:
+          console.log(authState, { authData })
+          break
+      }
+    })
+  },
+
+  methods: {
+    handleSignedIn(auth) {
+      // TODO: Salvar informações e buscar Workspaces.
+      console.log('handleSignedIn', auth)
+    },
+
+    handleSignedOut() {
+      // TODO: Implementar Logout.
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.external-auth__container {
+  height: 100vh;
 
+  .authenticator__container {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
 </style>
