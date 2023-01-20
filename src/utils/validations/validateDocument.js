e74b778d77e0318a06d34c279ce7096f02e77a96
Will Vitorino <josewilliam@justto.com.br>
2023-01-20 09:42:49 -0300

    🐛 Corrige bug da validação da documentos.

--- src/utils/validations/validateDocument.js
+++ src/utils/validations/validateDocument.js
@@ -2,22 +2,18 @@ import * as cpf from '@fnando/cpf'
 import * as cnpj from '@fnando/cnpj'
 
 const validateDocument = (rule, value, callback) => {
-  if (!value || !Object.keys(value).length) {
+  if (!value || !Object.keys(value).length || rule?.required === false) {
     callback()
     return 0
   }
+
   if (value instanceof Object) {
     value = value[Object.keys(value)[0]]
   }
-  if (value.length === 14) {
-    if (cpf.isValid(value)) {
-      callback()
-    } else callback(new Error())
-  } else {
-    if (cnpj.isValid(value)) {
-      callback()
-    } else callback(new Error())
-  }
+
+  const validator = value.length <= 14 ? cpf.isValid : cnpj.isValid
+
+  validator(value) ? callback() : callback(new Error(rule?.message || ''))
 }
 
 export default validateDocument
