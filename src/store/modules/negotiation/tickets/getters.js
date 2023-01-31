67cbe0a8c70f955fa91cb71b0b892999912dff96
Will Vitorino <josewilliam@justto.com.br>
2023-01-31 20:26:41 -0300

    SAAS-5541 ♻️ Não permitir esconder a listagem em workspaces com showNegotiationTypeMenu desabilitada.

--- src/store/modules/negotiation/tickets/getters.js
+++ src/store/modules/negotiation/tickets/getters.js
@@ -7,7 +7,7 @@ const ticketsGetters = {
   getTicketsActiveTab: state => state.ticketsActiveTab,
   getTicketsPrescriptions: state => state.ticketsQuery.prescriptions,
   getTicketsIsLoading: state => state.ticketsGettersCounter > 0 && state.ticketsQuery.page <= 2,
-  isToHideTickets: state => state.hideTickets
+  isToHideTickets: (state, getters) => getters.showNegotiationTypeMenu ? state.hideTickets : false
 }
 
 export default ticketsGetters
