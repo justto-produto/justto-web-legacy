e8cde9054ba8d78d8fc94188cddfe299fb8576ac
William Vitorino <josewilliam@justto.com.br>
2023-03-09 14:07:09 -0300

    Corrige handling de erros enviados para o Segment.

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -71,9 +71,9 @@ _axios.interceptors.response.use(
   },
   function(error) {
     if (process.env.NODE_ENV === 'production') Sentry.captureException(error)
-    /* if ([500, 403].includes(error.response?.status)) {
-      vue().$jusSegment('REQUEST_ERROR', error)
-    } */
+    if ([500, 403].includes(error.response?.status)) {
+      vue().$jusSegment('REQUEST_ERROR', { ...error })
+    }
     if (error.response?.status === 503) {
       showUnavailableLoading()
     }
