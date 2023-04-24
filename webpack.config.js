dde11a7f1d80937ae5c7084a78a65d03646bf359
William Vitorino <josewilliam@justto.com.br>
2023-04-24 18:35:47 -0300

    ⚰️ Remove editor Monaco.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,7 @@
 const CompressionPlugin = require('compression-webpack-plugin')
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 
 module.exports = {
-  entry: {
-    'css.worker': 'monaco-editor/esm/vs/language/css/css.worker',
-    'html.worker': 'monaco-editor/esm/vs/language/html/html.worker'
-  },
   plugins: [
-    new CompressionPlugin(),
-    new MonacoWebpackPlugin({
-      languages: ['javascript', 'css', 'html', 'typescript']
-    })
+    new CompressionPlugin()
   ]
 }
