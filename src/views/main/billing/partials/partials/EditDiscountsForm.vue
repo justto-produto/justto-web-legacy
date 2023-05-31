b7b4e9aa25dc418662489a8916e971f6ef7ff08e
Will Vitorino <josewilliam@justto.com.br>
2023-05-31 15:48:40 -0300

    SAAS-5746 ✨ Implementa criação/edição/remoção de descontos.

--- src/views/main/billing/partials/partials/EditDiscountsForm.vue
+++ src/views/main/billing/partials/partials/EditDiscountsForm.vue
@@ -2,7 +2,64 @@
   <el-form
     v-if="visible"
     :model="form"
-  />
+    size="mini"
+    class="discounts__form"
+  >
+    <el-row class="mb10">
+      <el-col :span="24">
+        <h3 class="mb0 mt0">
+          Descontos por importação
+        </h3>
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="8">
+      <el-col
+        v-for="(discount, dIndex) in form.discounts"
+        :key="`discount#${dIndex}`"
+        :span="24"
+      >
+        <SingleDiscountForm
+          v-model="form.discounts[dIndex]"
+          @delete="handleRemoveDiscount(dIndex)"
+        />
+      </el-col>
+
+      <el-col :span="24">
+        <el-button
+          type="primary"
+          size="mini"
+          icon="el-icon-plus"
+          @click="handleAddDiscount"
+        >
+          Adicionar desconto
+        </el-button>
+      </el-col>
+    </el-row>
+
+    <el-row>
+      <el-col>
+        <el-form-item class="flex-end">
+          <el-button @click="handleCancel">
+            Cancelar
+          </el-button>
+
+          <el-button
+            type="primary"
+            :disabled="loading"
+            @click="handleSave"
+          >
+            <i
+              v-show="loading"
+              class="el-icon-loading"
+            />
+
+            Salvar
+          </el-button>
+        </el-form-item>
+      </el-col>
+    </el-row>
+  </el-form>
 
   <DiscountsDescription
     v-else
@@ -14,11 +71,13 @@
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
+
 export default {
   name: 'EditDiscountsForm',
 
   components: {
-    DiscountsDescription: () => import('./partials/DiscountsDescription.vue')
+    DiscountsDescription: () => import('./partials/DiscountsDescription.vue'),
+    SingleDiscountForm: () => import('./partials/SingleDiscountForm')
   },
 
   props: {
@@ -56,21 +115,146 @@ export default {
   },
 
   methods: {
-    ...mapActions(['getContractDiscountList']),
+    ...mapActions([
+      'addContractDiscount',
+      'changeContractDiscount',
+      'deleteContractDiscount',
+      'getContractDiscountList'
+    ]),
+
+    handleUpdateDiscountsList() {
+      this.form.discounts = [...this.discounts]
+    },
+
+    handleShowLoading() {
+      this.loading = true
+    },
 
     init() {
       if (!this.contract?.id) return
 
-      // this.loading = true
+      this.handleShowLoading()
 
-      this.getContractDiscountList(this.contract?.id).then((discounts = []) => {
-        this.form.discounts = [...discounts]
+      this.getContractDiscountList(this.contract?.id).then(_ => {
+        this.handleUpdateDiscountsList()
+
+        if (this.form.discounts.length === 0) this.handleAddDiscount()
+      }).catch(error => this.$jusNotification({ error })).finally(() => { this.loading = false })
+    },
+
+    handleCancel() {
+      this.loading = false
+      this.visible = false
+    },
+
+    handleSave() {
+      this.handleShowLoading()
+
+      const promises = this.form.discounts.map(discount => ({
+        fn: discount?.id ? this.changeContractDiscount : this.addContractDiscount,
+        payload: { contractId: this.contract.id, discountId: discount?.id, discount }
+      }))
+
+      Promise.all(promises.map(({ fn, payload }) => fn(payload))).then(async _ => {
+        this.$jusNotification({
+          type: 'success',
+          title: 'Yay!',
+          message: 'Contrato editado com sucesso.'
+        })
+
+        this.handleUpdateDiscountsList()
+        this.handleCancel()
       }).catch(error => {
         this.$jusNotification({ error })
       }).finally(() => {
         this.loading = false
       })
+    },
+
+    handleAddDiscount() {
+      this.form.discounts.push({
+        minVolume: 0,
+        value: 0,
+        type: 'MONTHLY_SUBSCRIPTION_VALUE'
+      })
+    },
+
+    handleRemoveDiscount(index) {
+      if (this.form.discounts[index]?.id) {
+        this.handleShowLoading()
+
+        this.deleteContractDiscount({
+          contractId: this.contract.id,
+          discountId: this.form.discounts[index]?.id
+        })
+          .then(this.handleUpdateDiscountsList)
+          .catch(error => this.$jusNotification({ error }))
+          .finally(() => { this.loading = false })
+      } else {
+        this.form.discounts.splice(index, 1)
+      }
     }
   }
 }
 </script>
+
+<style lang="scss">
+.discounts__form {
+  .el-row {
+    .el-col {
+      .el-form-item.flex-item {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+
+        .el-form-item__content {
+          width: 100%;
+
+          .el-form--inline {
+            .el-row {
+              .el-col {
+                .el-form-item {
+                  width: 100% !important;
+                  line-height: 0;
+
+                  .el-form-item__label {
+                    font-weight: normal !important;
+                    line-height: 1.5rem;
+                  }
+
+                  .el-form-item__content {
+                    .el-input-number {
+                      width: 100% !important;
+                    }
+                  }
+                }
+
+                .labelless {
+                  .el-form-item__label  {
+                    visibility: hidden;
+                  }
+                }
+              }
+            }
+          }
+        }
+
+        &.small-label {
+          .el-form-item__label {
+            font-size: 1rem !important;
+          }
+        }
+      }
+
+      .el-form-item.flex-end {
+        margin: 0;
+
+        .el-form-item__content {
+          display: flex;
+          justify-content: flex-end;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/main/billing/partials/partials/partials/SingleDiscountForm.vue
@@ -0,0 +1,89 @@
+<template>
+  <el-form
+    :inline="true"
+    :model="value"
+    size="mini"
+    class="discount-inline__form"
+  >
+    <el-row :gutter="8">
+      <el-col :span="10">
+        <el-form-item label="Volume de importações">
+          <el-input-number
+            v-model="value.minVolume"
+            :min="0"
+            step-strictly
+            controls-position="right"
+          />
+        </el-form-item>
+      </el-col>
+
+      <el-col :span="10">
+        <el-form-item label="Valor do desconto">
+          <div class="el-input el-input--mini">
+            <money
+              v-model="value.value"
+              class="el-input__inner"
+            />
+          </div>
+        </el-form-item>
+      </el-col>
+
+      <el-col :span="4">
+        <el-button
+          type="danger"
+          icon="el-icon-delete"
+          class="mt20"
+          plain
+          size="mini"
+          @click="$emit('delete', value.type)"
+        />
+      </el-col>
+    </el-row>
+  </el-form>
+</template>
+
+<script>
+export default {
+  name: 'SingleDiscountForm',
+
+  props: {
+    value: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.discount-inline__form {
+  line-height: 0 !important;
+
+  .el-row {
+    line-height: 0 !important;
+
+    .el-col {
+      line-height: 0 !important;
+
+      .el-form-item {
+        margin: 0 !important;
+        width: 100% !important;
+        line-height: 0;
+
+        .el-form-item__label {
+          font-weight: normal !important;
+          line-height: 1.5rem;
+        }
+
+        .el-form-item__content {
+          line-height: 0 !important;
+
+          .el-input-number {
+            width: 100% !important;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
