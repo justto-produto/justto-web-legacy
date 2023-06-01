ac021e2240b50299376a9e420d6bd34f8e261322
Will Vitorino <josewilliam@justto.com.br>
2023-06-01 20:02:23 -0300

    SAAS-5751 ♻️ Implementa atribuição dos dados corretos ao fields, dependendo do que vem do formulário.

--- src/views/main/configurations/partials/FeaturesAndModulesDialogs/partials/ApiIntegrationForm.vue
+++ src/views/main/configurations/partials/FeaturesAndModulesDialogs/partials/ApiIntegrationForm.vue
@@ -28,8 +28,59 @@ export default {
   }),
 
   methods: {
-    handleInitIntegration(url) {
-      // const integrationType = (url.toLowerCase()).includes()
+    handleVerifyIntegrationType(url) {
+      const urlLowerCase = url.toLowerCase()
+      const isProjurisSoapActive = urlLowerCase.includes('projuris')
+      const isFinchActive = urlLowerCase.includes('finch')
+
+      const type = isProjurisSoapActive ? 'PROJURIS_SOAP' : isFinchActive ? 'FINCH' : 'JUSTTO_WEBHOOK'
+
+      // TODO: Confirmar se o tipo está correto.
+      // TODO: Caso não esteja, perguntar qual o tipo correto.
+
+      this.handleInitIntegration(url, type)
+    },
+
+    handleInitIntegration(url, type) {
+      switch (type) {
+        case 'PROJURIS_SOAP':
+          this.handleInitProjurisIntegration(url)
+          break
+        case 'FINCH':
+          this.initFinchIntegration(url)
+          break
+        default:
+          this.initJusttoIntegration(url)
+          break
+      }
+    },
+
+    handleInitProjurisIntegration(url) {
+      this.fields = {
+        PROJURIS_SOAP_ACTIVE: true,
+        PROJURIS_SOAP_PASSWORD: '',
+        PROJURIS_SOAP_TOKEN: '',
+        PROJURIS_SOAP_URL: url,
+        PROJURIS_SOAP_USERNAME: ''
+      }
+    },
+
+    initFinchIntegration(url) {
+      this.fields = {
+        FINCH_ACTIVE: true,
+        FINCH_ENDPOINT: url,
+        FINCH_USERNAME: '',
+        FINCH_PASSWORD: ''
+      }
+    },
+
+    initJusttoIntegration(url) {
+      this.fields = {
+        JUSTTO_WEBHOOK_ACTIVE: true,
+        JUSTTO_WEBHOOK_ENDPOINT: url,
+        JUSTTO_WEBHOOK_USERNAME: '',
+        JUSTTO_WEBHOOK_PASSWORD: ''
+      }
     }
   }
 }
